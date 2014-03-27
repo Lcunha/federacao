fb4c5cf99673749e9fa5ab7f9153e9e2ca6186e5
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-03-27 14:40:17 -0300

    Added descriptions comment in funcitons of persistence/TecnicoDAO.php. [Style and Design]

--- persistence/TecnicoDAO.php
+++ persistence/TecnicoDAO.php
@@ -10,10 +10,16 @@ include_once (__APP_PATH.'/persistence/Conexao.php');
 class TecnicoDAO{
 	private $conexao;
 	
+	/*
+		The method is responsibility to instance the class.
+	*/
 	public function __construct(){
 		$this->conexao = new Conexao();
 	}
 	
+	/*
+		The method is responsibility to list all registers in database table.
+	*/
 	public function listarTodos(){
 		$sql = "SELECT * FROM tecnico";
 		$resultado = $this->conexao->banco->Execute($sql);
@@ -25,17 +31,26 @@ class TecnicoDAO{
 		return $retornaTecnico;
 	}
 	
+	/*
+		The method is responsibility to insert data in database table.
+	*/
 	public function inserir(Tecnico $dadosTecnico){
 		$sql = "INSERT INTO tecnico (nome,telefone,cpf) VALUES ('{$dadosTecnico->__getNome()}','{$dadosTecnico->__getTelefone()}','{$dadosTecnico->__getCpf()}')";
 		$this->conexao->banco->Execute($sql);
 	}
 	
+	/*
+		The method is responsibility to update data in database table.
+	*/
 	public function atualizar(Tecnico $dadosTecnico){
 		$sql = "UPDATE tecnico SET nome='{$dadosTecnico->__getNome()}', telefone='{$dadosTecnico->__getTelefone()}', cpf='{$dadosTecnico->__getCpf()}' WHERE id_tecnico='{$dadosTecnico->__getIdTecnico()}' ";
 		$this->conexao->banco->Execute($sql);
 		return $dadosTecnico;
 	}
 	
+	/*
+		The method is responsibility to consult in database table through of ID.
+	*/
 	public function consultarPorId($id){
 		$sql = "SELECT * FROM tecnico WHERE id_tecnico= '{$id}'";
 		$resultado = $this->conexao->banco->Execute($sql);
@@ -45,6 +60,9 @@ class TecnicoDAO{
 		return $dadosTecnico;
 	}
 	
+	/*
+		The method is responsibility to consult in database table through of Name.
+	*/
 	public function consultarPorNome($nome){
 		$sql = "SELECT * FROM tecnico WHERE nome= '{$nome}'";
 		$resultado = $this->conexao->banco->Execute($sql);
@@ -53,10 +71,18 @@ class TecnicoDAO{
 		$dadosTecnico->__constructOverload($registro->ID_TECNICO,$registro->NOME,$registro->TELEFONE,$registro->CPF);
 		return $dadosTecnico;
 	}
+
+	/*
+		The method is responsibility to delete data in database table.
+	*/
 	public function excluir($id){
 		$sql = "DELETE FROM tecnico WHERE id_tecnico= '{$id}' ";
 		$resultado = $this->conexao->banco->Execute($sql);
 	}
+
+	/*
+		The method is responsibility to count registers of Coach in database table. 
+	*/
 	public function contarRegistrosTecnico(){
 		$sql = "SELECT COUNT(*) as contagem FROM tecnico";
 		$resultado = $this->conexao->banco->Execute($sql);
