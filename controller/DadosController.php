77f3fbf09ffd279524edafb26ce9349afdd0d41f
Larissa <larissax55@gmail.com>
2014-03-26 23:16:02 -0300

    Indentation[style and desing]

--- controller/DadosController.php
+++ controller/DadosController.php
@@ -1,7 +1,8 @@
 <?php
+
 /*
   Name:  DadosControle.php
-  Description:Class that performs the listing of teams 
+  Description:Class that performs the listing of teams
   and games already registered in the system in a table may exluir, edit, update and save.
  */
 
@@ -11,115 +12,116 @@ include_once('./model/Jogador.php');
 include_once('./controller/JogadorController.php');
 include_once('./controller/TimeController.php');
 
-class DadosController{
-	
-	private $dadosDAO;
-	
-	public function __construct(){
-		$this->dadosDAO = new DadosDAO();
-	}
-        
-	public function _listarDadosParaTabela(){
-            /*
-             Function responsible for receiving all game data reported by the User System.
-             */
-		$dadosDados = new Dados();
-		$arrayDados = $this->dadosDAO->listarTodos();
-		for($i=0; $i<count($arrayDados); $i++){
-			$dadosDados = $arrayDados[$i];
-			$arrayTr[] = "
+class DadosController {
+
+    private $dadosDAO;
+
+    public function __construct() {
+        $this->dadosDAO = new DadosDAO();
+    }
+
+    public function _listarDadosParaTabela() {
+        /*
+          Function responsible for receiving all game data reported by the User System.
+         */
+        $dadosDados = new Dados();
+        $arrayDados = $this->dadosDAO->listarTodos();
+        for ($i = 0; $i < count($arrayDados); $i++) {
+            $dadosDados = $arrayDados[$i];
+            $arrayTr[] = "
 			<tr>
           			<td><input type=\"checkbox\" value=\"1\" name=\"marcar[]\" /></td>
-					<td>".$dadosDados->__getIdDados()."</td>
-          			<td>".$dadosDados->__getIdJogador()."</td>
-          			<td>".$dadosDados->__getIdTempo()."</td>
-          			<td>".$dadosDados->__getAdvertencia()."</td>
-          			<td>".$dadosDados->__getPunicao()."</td>
-          			<td>".$dadosDados->__getDesqualificacao()."</td>
-          			<td>".$dadosDados->__getRelatorio()."</td>
-          			<td>".$dadosDados->__getGol()."</td>		
+					<td>" . $dadosDados->__getIdDados() . "</td>
+          			<td>" . $dadosDados->__getIdJogador() . "</td>
+          			<td>" . $dadosDados->__getIdTempo() . "</td>
+          			<td>" . $dadosDados->__getAdvertencia() . "</td>
+          			<td>" . $dadosDados->__getPunicao() . "</td>
+          			<td>" . $dadosDados->__getDesqualificacao() . "</td>
+          			<td>" . $dadosDados->__getRelatorio() . "</td>
+          			<td>" . $dadosDados->__getGol() . "</td>		
           			<td>
-            			<a href=\"?pag=dados&action=edit&id=".$dadosDados->__getIdDados()."\"><img src=\"./views/images/edit.png\" width=\"16\" height=\"16\" /></a>
-            			<a href=\"?pag=dados&action=exclude&id=".$dadosDados->__getIdDados()."\"><img src=\"./views/images/delete.png\" width=\"16\" height=\"16\" /></a>
+            			<a href=\"?pag=dados&action=edit&id=" . $dadosDados->__getIdDados() . "\"><img src=\"./views/images/edit.png\" width=\"16\" height=\"16\" /></a>
+            			<a href=\"?pag=dados&action=exclude&id=" . $dadosDados->__getIdDados() . "\"><img src=\"./views/images/delete.png\" width=\"16\" height=\"16\" /></a>
           			</td>
 			</tr>";
-		}
-		return $arrayTr;
-	}
-        
-	public function _listarTodos(){
-		return $this->dadosDAO->listarTodos();
-	}
-        
-	public function _consultarPorId($id){
-            /*
-             Makes a query data stored games from an id entered by the User.
-             */
-		$dadosDados = new Dados();
-		$dadosDados =  $this->dadosDAO->consultarPorId($id);
-		$arrayDados['advertencia'] = $dadosDados->__getAdvertencia();
-		$arrayDados['punicao'] = $dadosDados->__getPunicao();
-		$arrayDados['desqualificacao'] = $dadosDados->__getDesqualificacao();
-		$arrayDados['relatorio'] = $dadosDados->__getRelatorio();
-		
-		return $arrayDados;
-	}
-        
-	public function _consultarPorRelatorio($relatorio){
-            /*
-              Makes a query of reports of games already registered.
-             */
-		$dadosDados = new Dados();
-		$dadosDados = $this->dadosDAO->consultarPorRelatorio($relatorio);
-		return $dadosDados;
-	}
-        
-	public function _inserir($idTempoInserido,$idTimeA,$idTimeB){
-            /*
-               Inserts the data team in a match.
-             */
-		$dadosTimeA = new Dados();
-		$dadosTimeB = new Dados();
-		$timeCO = new TimeController();
-		$timeA = $timeCO->_listarTodosJogadores($idTimeA);
-		$timeB = $timeCO->_listarTodosJogadores($idTimeB);
-		
-		for($i=0; $i<count($timeA); $i++){
-			$dadosTimeA->__constructOverload(0, $timeA[$i]->__getIdJogador() , $idTempoInserido, 0,0,0,0,0);
-			$this->dadosDAO->inserir($dadosTimeA);
-		}
-		for($i=0; $i<count($timeB); $i++){
-			$dadosTimeB->__constructOverload(0, $timeB[$i]->__getIdJogador() , $idTempoInserido, 0,0,0,0,0);
-			$this->dadosDAO->inserir($dadosTimeB);
-		}
-	}
-        
-	public function _atualizar($idDados, $idJogador, $idTempo, $advertencia,
-                $punicao, $desqualificacao, $relatorio, $gol){
-          /*
-              
-           
-           */           
-		
-                $dadosDados = new Dados();
-		$dadosDados->__constructOverload($idDados, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, 
-                $relatorio, $gol);
-		$this->dadosDAO->atualizar($dadosDados);
-	}
-        
-	public function _atualizarDados($idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol){
-		$dadosDados = new Dados();
-		$dadosDados->__constructOverload(0, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol);
-		$this->dadosDAO->atualizarDados($dadosDados);
-	}
-        
-	public function _salvar($idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio){
-		$dadosDados= new Dados();
-		$dadosDados->__constructOverload(0,$idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio);
-		$this->dadosDAO->inserir($dadosDados);
-	}
-        
-	public function _excluir($id){
-		return $this->dadosDAO->excluir($id);
-	}
+        }
+        return $arrayTr;
+    }
+
+    public function _listarTodos() {
+        return $this->dadosDAO->listarTodos();
+    }
+
+    public function _consultarPorId($id) {
+        /*
+          Makes a query data stored games from an id entered by the User.
+         */
+        $dadosDados = new Dados();
+        $dadosDados = $this->dadosDAO->consultarPorId($id);
+        $arrayDados['advertencia'] = $dadosDados->__getAdvertencia();
+        $arrayDados['punicao'] = $dadosDados->__getPunicao();
+        $arrayDados['desqualificacao'] = $dadosDados->__getDesqualificacao();
+        $arrayDados['relatorio'] = $dadosDados->__getRelatorio();
+
+        return $arrayDados;
+    }
+
+    public function _consultarPorRelatorio($relatorio) {
+        /*
+          Makes a query of reports of games already registered.
+         */
+        $dadosDados = new Dados();
+        $dadosDados = $this->dadosDAO->consultarPorRelatorio($relatorio);
+        return $dadosDados;
+    }
+
+    public function _inserir($idTempoInserido, $idTimeA, $idTimeB) {
+        /*
+          Inserts the data team in a match.
+         */
+        $dadosTimeA = new Dados();
+        $dadosTimeB = new Dados();
+        $timeCO = new TimeController();
+        $timeA = $timeCO->_listarTodosJogadores($idTimeA);
+        $timeB = $timeCO->_listarTodosJogadores($idTimeB);
+
+        for ($i = 0; $i < count($timeA); $i++) {
+            $dadosTimeA->__constructOverload(0, $timeA[$i]->__getIdJogador(), $idTempoInserido, 0, 0, 0, 0, 0);
+            $this->dadosDAO->inserir($dadosTimeA);
+        }
+        for ($i = 0; $i < count($timeB); $i++) {
+            $dadosTimeB->__constructOverload(0, $timeB[$i]->__getIdJogador(), $idTempoInserido, 0, 0, 0, 0, 0);
+            $this->dadosDAO->inserir($dadosTimeB);
+        }
+    }
+
+    public function _atualizar($idDados, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol) {
+        /*
+          Responsible for updating the data of the games making the necessary changes to them.
+         */
+        $dadosDados = new Dados();
+        $dadosDados->__constructOverload($idDados, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol);
+        $this->dadosDAO->atualizar($dadosDados);
+    }
+
+    public function _atualizarDados($idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol) {
+
+        /*
+          Responsible for updating all the data matches.
+         */
+        $dadosDados = new Dados();
+        $dadosDados->__constructOverload(0, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio, $gol);
+        $this->dadosDAO->atualizarDados($dadosDados);
+    }
+
+    public function _salvar($idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio) {
+        $dadosDados = new Dados();
+        $dadosDados->__constructOverload(0, $idJogador, $idTempo, $advertencia, $punicao, $desqualificacao, $relatorio);
+        $this->dadosDAO->inserir($dadosDados);
+    }
+
+    public function _excluir($id) {
+        return $this->dadosDAO->excluir($id);
+    }
+
 }
--- controller/DadosTimeController.php
+++ controller/DadosTimeController.php
@@ -1,62 +1,67 @@
 <?php
+
 /*
-  Name: DadosTimeController.php    
-  Description: Updates the points earned by teams in each match held. 
+  Name: DadosTimeController.php
+  Description: Updates the points earned by teams in each match held.
  */
 
 include_once('./persistence/DadosTimeDAO.php');
 include_once('./model/DadosTime.php');
 
-class DadosTimeController{
+class DadosTimeController {
 
-        private $dadosTimeDAO;
-	
-	public function __construct(){
-		$this->dadosTimeDAO = new DadosTimeDAO();
-	}
-        
-	public function _listarTodos(){
-		return $this->dadosTimeDAO->listarTodos();
-	}
-        
-	public function _consultarPorId($id){
-		$dadosDadosTime = new DadosTime();
-		$dadosDadosTime =  $this->dadosTimeDAO->consultarPorId($id);
-		//$arrayDados['nome'] = $dadosArbitro->__getNome();
-		//$arrayDados['telefone'] = $dadosArbitro->__getTelefone();
-		//$arrayDados['cpf'] = $dadosArbitro->__getCpf();
-	
-		return $arrayDados;
-	}
-        
-	public function _inserir(DadosTime $dadosTime){
-		return $this->dadosTimeDAO->inserir($dadosTime);
-	}
-        
-	public function _atualizar($idDadosTime, $pontos, $jogos, $vitorias, $empates, $derrotas, $gols, $golsLevados){
-		$dadosDadosTime = new DadosTime();
-		$dadosDadosTime->__constructOverload(0, $idDadosTime, $pontos, $jogos, $vitorias, $empates, $derrotas,
-                $gols, $golsLevados);
-		$this->dadosTimeDAO->atualizar($dadosDadosTime);
-	}
-        
-	public function _atualizarPontos($idTimeA, $idTimeB, $golsTimeA, $golsTimeB){
-		if($golsTimeA > $golsTimeB){
-			$pontuacaoA = 3;
-			$pontuacaoB = 0;
-		}
-		else if($golsTimeA < $golsTimeB){
-			$pontuacaoA = 0;
-			$pontuacaoB = 3;
-		}
-		else if($golsTimeA == $golsTimeB){
-			$pontuacaoA = 1;
-			$pontuacaoB = 1;
-		}
-		$this->dadosTimeDAO->atualizarPontos($idTimeA, $idTimeB, $pontuacaoA, $pontuacaoB, $golsTimeA, $golsTimeB);
-	}
-        
-	public function _excluir($id){
-		return $this->dadosTimeDAO->excluir($id);
-	}
+    private $dadosTimeDAO;
+
+    public function __construct() {
+        $this->dadosTimeDAO = new DadosTimeDAO();
+    }
+
+    public function _listarTodos() {
+        return $this->dadosTimeDAO->listarTodos();
+    }
+
+    public function _consultarPorId($id) {
+        $dadosDadosTime = new DadosTime();
+        $dadosDadosTime = $this->dadosTimeDAO->consultarPorId($id);
+        //$arrayDados['nome'] = $dadosArbitro->__getNome();
+        //$arrayDados['telefone'] = $dadosArbitro->__getTelefone();
+        //$arrayDados['cpf'] = $dadosArbitro->__getCpf();
+
+        return $arrayDados;
+    }
+
+    public function _inserir(DadosTime $dadosTime) {
+        return $this->dadosTimeDAO->inserir($dadosTime);
+    }
+
+    public function _atualizar($idDadosTime, $pontos, $jogos, $vitorias, $empates, $derrotas, $gols, $golsLevados) {
+        /*
+         Responsible for updating the data team.
+         */
+        $dadosDadosTime = new DadosTime();
+        $dadosDadosTime->__constructOverload(0, $idDadosTime, $pontos, $jogos, $vitorias, $empates, $derrotas, $gols, $golsLevados);
+        $this->dadosTimeDAO->atualizar($dadosDadosTime);
+    }
+
+    public function _atualizarPontos($idTimeA, $idTimeB, $golsTimeA, $golsTimeB) {
+       /*
+        Responsible for updating the scores of times in matches.
+        */
+        if ($golsTimeA > $golsTimeB) {
+            $pontuacaoA = 3;
+            $pontuacaoB = 0;
+        } else if ($golsTimeA < $golsTimeB) {
+            $pontuacaoA = 0;
+            $pontuacaoB = 3;
+        } else if ($golsTimeA == $golsTimeB) {
+            $pontuacaoA = 1;
+            $pontuacaoB = 1;
+        }
+        $this->dadosTimeDAO->atualizarPontos($idTimeA, $idTimeB, $pontuacaoA, $pontuacaoB, $golsTimeA, $golsTimeB);
+    }
+
+    public function _excluir($id) {
+        return $this->dadosTimeDAO->excluir($id);
+    }
+
 }
