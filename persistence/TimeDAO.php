1ea48a434ff1e8845e04944b8292aba4c429b73f
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-03-28 11:22:17 -0300

    Added indentation rulers in persistence/TimeDAO.php [Style and Design]

--- persistence/TimeDAO.php
+++ persistence/TimeDAO.php
@@ -1,9 +1,11 @@
 <?php
+
 /*
 	Name: TimeDAO.php  
-	Description:Class data persistence with CRUD functions (create, read, update, delete) 
-	for handling the type Team, in the relevant table in mysql.
- */
+	Description:Class data persistence with CRUD functions (create, read, 
+	update, delete) for handling the type Team, in the relevant table in mysql.
+*/
+
 include_once ('./model/Time.php');
 include_once ('./model/Tecnico.php');
 include_once ('./model/Jogador.php');
@@ -15,52 +17,64 @@ class TimeDAO{
 	private $conexao;
 	
 	/*
-		The method is responsibility by to instance the class and initialize conection with database.
+		The method is responsibility by to instance the class and initialize
+		conection with database.
 	*/
 	public function __construct(){
 		$this->conexao = new Conexao();
 	}
 	
 	/*
-		The method is responsibility by to list all registers in database table of type Team.
+		The method is responsibility by to list all registers in database table
+		of type Team.
 	*/
 	public function listarTodos(){
 		$sql = "SELECT * FROM time";
 		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject()){
 			$dadosTime = new Time();
-			$dadosTime->__constructOverload($registro->ID_TIME,$registro->TECNICO_ID_TECNICO,$registro->NOME,$registro->CATEGORIA,$registro->ENDERECO,$registro->DATA_FUNDACAO,$registro->PRESIDENTE,$registro->TELEFONE);
+			$dadosTime->__constructOverload($registro->ID_TIME, $registro->TECNICO_ID_TECNICO,
+											$registro->NOME,$registro->CATEGORIA, $registro->ENDERECO, 
+											$registro->DATA_FUNDACAO, $registro->PRESIDENTE, $registro->TELEFONE);
 			$retornaTime[] = $dadosTime;
 		}
 		return $retornaTime;
 	}
 
 	/*
-		The method is responsibility by to list all registers in database table of type Player.
+		The method is responsibility by to list all registers in database table 
+		of type Player.
 	*/
 	public function listarTodosJogadores($idTime){
 		$sql = "SELECT * FROM jogador WHERE time_id_time = '{$idTime}'";
 		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject()){
 			$dadosJogador = new Jogador();
-			$dadosJogador->__constructOverload($registro->ID_JOGADOR,$registro->TIME_ID_TIME,$registro->NOME,$registro->DATA_NASCIMENTO,$registro->CPF,$registro->NUMERO);
+			$dadosJogador->__constructOverload($registro->ID_JOGADOR, $registro->TIME_ID_TIME,
+											   $registro->NOME, $registro->DATA_NASCIMENTO,
+											   $registro->CPF, $registro->NUMERO);
 			$retornaJogador[] = $dadosJogador;
 		}
 		return $retornaJogador;
 	}
 
 	/*
-		The method is responsibility by to list all registers im database table through type of campionship data and team.
+		The method is responsibility by to list all registers im database table
+		through type of campionship data and team.
 	*/
 	public function listarTodosPorPontos(){
-		$sql = "SELECT t.id_time,dc.pontos,dc.jogos,dc.vitorias,dc.empates,dc.derrotas,dc.gols,dc.gols_levados
+		$sql = "SELECT t.id_time, dc.pontos, dc.jogos, dc.vitorias, dc.empates, 
+					   dc.derrotas, dc.gols, dc.gols_levados
 				FROM dados_campeonato dc,time t
 				WHERE t.id_time = dc.time_id_time
 				ORDER BY dc.pontos DESC;";
 		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject()){
 			$dadosTime = new DadosTime();
-			$dadosTime->__constructOverload(0,$registro->ID_TIME,$registro->PONTOS,$registro->JOGOS,$registro->VITORIAS,$registro->EMPATES,$registro->DERROTAS,$registro->GOLS,$registro->GOLS_LEVADOS);
+			$dadosTime->__constructOverload(0, $registro->ID_TIME, $registro->PONTOS, 
+											$registro->JOGOS, $registro->VITORIAS,
+											$registro->EMPATES,$registro->DERROTAS,
+											$registro->GOLS,$registro->GOLS_LEVADOS);
 			$retornaTime[] = $dadosTime;
 		}
 		return $retornaTime;
@@ -70,7 +84,12 @@ class TimeDAO{
 		The method is responsibility by to insert data in database table.
 	*/
 	public function inserir(Time $dadosTime){
-		$sql = "INSERT INTO time (tecnico_id_tecnico,nome,categoria,endereco,data_fundacao,presidente,telefone) VALUES ('{$dadosTime->__getIdTecnico()}','{$dadosTime->__getNome()}','{$dadosTime->__getCategoria()}','{$dadosTime->__getEndereco()}','{$dadosTime->__getDataFundacao()}','{$dadosTime->__getPresidente()}','{$dadosTime->__getTelefone()}')";
+		$sql = "INSERT INTO time (tecnico_id_tecnico, nome, categoria, endereco, 
+							data_fundacao, presidente, telefone) 
+				VALUES ('{$dadosTime->__getIdTecnico()}',
+						'{$dadosTime->__getNome()}', '{$dadosTime->__getCategoria()}', 
+						'{$dadosTime->__getEndereco()}', '{$dadosTime->__getDataFundacao()}', 
+						'{$dadosTime->__getPresidente()}', '{$dadosTime->__getTelefone()}')";
 		$this->conexao->banco->Execute($sql);
 		return mysql_insert_id();
 	}
@@ -79,7 +98,14 @@ class TimeDAO{
 		The method is responsibility by to update data in database table.
 	*/
 	public function atualizar(Time $dadosTime){
-		$sql = "UPDATE time SET tecnico_id_tecnico='{$dadosTime->__getIdTecnico()}',nome='{$dadosTime->__getNome()}', categoria='{$dadosTime->__getCategoria()}', endereco='{$dadosTime->__getEndereco()}', data_fundacao='{$dadosTime->__getDataFundacao()}', presidente='{$dadosTime->__getPresidente()}', telefone='{$dadosTime->__getTelefone()}' WHERE id_time='{$dadosTime->__getIdTime()}' ";
+		$sql = "UPDATE time 
+				SET tecnico_id_tecnico = '{$dadosTime->__getIdTecnico()}',
+					nome = '{$dadosTime->__getNome()}', categoria = '{$dadosTime->__getCategoria()}', 
+					endereco = '{$dadosTime->__getEndereco()}', 
+					data_fundacao = '{$dadosTime->__getDataFundacao()}', 
+					presidente = '{$dadosTime->__getPresidente()}', 
+					telefone = '{$dadosTime->__getTelefone()}' 
+				WHERE id_time = '{$dadosTime->__getIdTime()}' ";
 		$this->conexao->banco->Execute($sql);
 		return $dadosTime;
 	}
@@ -88,11 +114,13 @@ class TimeDAO{
 		The method is responsibility by to consult in database table through of ID.
 	*/
 	public function consultarPorId($id){
-		$sql = "SELECT * FROM time WHERE id_time= '{$id}'";
+		$sql = "SELECT * FROM time WHERE id_time = '{$id}'";
 		$resultado = $this->conexao->banco->Execute($sql);
 		$registro = $resultado->FetchNextObject();
 		$dadosTime = new Time();
-		$dadosTime->__constructOverload($registro->ID_TIME,$registro->TECNICO_ID_TECNICO,$registro->NOME,$registro->CATEGORIA,$registro->ENDERECO,$registro->DATA_FUNDACAO,$registro->PRESIDENTE,$registro->TELEFONE);
+		$dadosTime->__constructOverload($registro->ID_TIME, $registro->TECNICO_ID_TECNICO, 
+										$registro->NOME, $registro->CATEGORIA, $registro->ENDERECO, 
+										$registro->DATA_FUNDACAO, $registro->PRESIDENTE, $registro->TELEFONE);
 		return $dadosTime;
 	}
 	
@@ -100,11 +128,14 @@ class TimeDAO{
 		The method is responsibility by to consult in database table through of Name.
 	*/
 	public function consultarPorNome($nome){
-		$sql = "SELECT * FROM time WHERE nome= '{$nome}'";
+		$sql = "SELECT * FROM time WHERE nome = '{$nome}'";
 		$resultado = $this->conexao->banco->Execute($sql);
 		$registro = $resultado->FetchNextObject();
 		$dadosTime = new Time();
-		$dadosTime->__constructOverload($registro->ID_TIME,$registro->TECNICO_ID_TECNICO,$registro->NOME,$registro->CATEGORIA,$registro->ENDERECO,$registro->DATA_FUNDACAO,$registro->PRESIDENTE,$registro->TELEFONE,$registro->PONTOS);
+		$dadosTime->__constructOverload($registro->ID_TIME, $registro->TECNICO_ID_TECNICO,
+										$registro->NOME, $registro->CATEGORIA, $registro->ENDERECO, 
+										$registro->DATA_FUNDACAO, $registro->PRESIDENTE, $registro->TELEFONE, 
+										$registro->PONTOS);
 		return $dadosTime;
 	}
 
@@ -112,7 +143,7 @@ class TimeDAO{
 		The method is responsibility by to delete data in database table.
 	*/
 	public function excluir($id){
-		$sql = "DELETE FROM time WHERE id_time= '{$id}' ";
+		$sql = "DELETE FROM time WHERE id_time = '{$id}' ";
 		$resultado = $this->conexao->banco->Execute($sql);
 	}
 
@@ -126,3 +157,4 @@ class TimeDAO{
 		return $registro->CONTAGEM;
 	}
 }
+
