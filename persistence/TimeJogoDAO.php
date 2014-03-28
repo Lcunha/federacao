f91dd086c51569d1306e6157b747cc0acbde0d20
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-03-28 11:00:25 -0300

    Added indentation rulers in persistence/TimeJogoDAO.php [Style and Design]

--- persistence/TimeJogoDAO.php
+++ persistence/TimeJogoDAO.php
@@ -1,8 +1,9 @@
 <?php
 /*
 	Name: TimeJogoDAO.php  
-	Description:Class data persistence with CRUD functions (create, read, update, delete) 
-	for handling the associative type Team_Game, in the relevant table (table associative) in mysql.
+	Description:Class data persistence with CRUD functions (create, read, update,
+	delete) for handling the associative type Team_Game, in the relevant table 
+	(table associative) in mysql.
  */
 include_once (__APP_PATH.'/model/Time.php');
 include_once (__APP_PATH.'/model/Jogo.php');
@@ -12,7 +13,8 @@ class TimeJogoDAO{
 	private $conexao;
 	
 	/*
-		The method is responsibility by to instance the class and initialize conection with database.
+		The method is responsibility by to instance the class and initialize 
+		conection with database.
 	*/
 	public function __construct(){
 		$this->conexao = new Conexao();
@@ -26,7 +28,7 @@ class TimeJogoDAO{
 		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject()){
 			$dadosTimeJogo = new TimeJogo();
-			$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME,$registro->TEMPO_ID_TEMPO);
+			$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME, $registro->TEMPO_ID_TEMPO);
 			$retornaTimeJogo[] = $dadosTimeJogo;
 		}
 		return $retornaTimeJogo;
@@ -36,7 +38,8 @@ class TimeJogoDAO{
 		The method is responsiblity by to insert data in database table.
 	*/
 	public function inserir(TimeJogo $dadosTimeJogo){
-		$sql = "INSERT INTO jogo_time (jogo_id_jogo,time_id_time) VALUES ('{$dadosTimeJogo->__getIdJogo()}','{$dadosTimeJogo->__getIdTime()}')";
+		$sql = "INSERT INTO jogo_time (jogo_id_jogo,time_id_time) VALUES 
+		('{$dadosTimeJogo->__getIdJogo()}', '{$dadosTimeJogo->__getIdTime()}')";
 		$this->conexao->banco->Execute($sql);
 	}
 
@@ -44,7 +47,8 @@ class TimeJogoDAO{
 		The method is responsibility by to update data in database table.
 	*/
 	public function atualizar(TimeJogo $dadosTimeJogo){
-		$sql = "UPDATE jogo_time SET jogo_id_jogo='{$dadosTimeJogo->__getIdJogo()}', time_id_time='{$dadosTimeJogo->__getIdTime()}' ";
+		$sql = "UPDATE jogo_time SET jogo_id_jogo = '{$dadosTimeJogo->__getIdJogo()}',
+		time_id_time = '{$dadosTimeJogo->__getIdTime()}' ";
 		$this->conexao->banco->Execute($sql);
 	}
 
@@ -52,11 +56,11 @@ class TimeJogoDAO{
 		The method is responsiblity by to consult in database table through of type Team.
 	*/
 	public function consultarPorTime($idTime){
-		$sql = "SELECT * FROM time_jogo WHERE time_id_time= '{$idTime}'";
+		$sql = "SELECT * FROM time_jogo WHERE time_id_time = '{$idTime}'";
 		$resultado = $this->conexao->banco->Execute($sql);
 		$registro = $resultado->FetchNextObject();
 		$dadosTimeJogo = new TimeJogo();
-		$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME,$registro->TEMPO_ID_TEMPO);
+		$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME, $registro->TEMPO_ID_TEMPO);
 		return $dadosTimeJogo;
 	}
 	
@@ -64,11 +68,11 @@ class TimeJogoDAO{
 		The method is responsiblity by to consult in database table through Time.
 	*/
 	public function consultarPorTempo($idTempo){
-			$sql = "SELECT * FROM time_jogo WHERE tempo_id_tempo= '{$idTempo}'";
+			$sql = "SELECT * FROM time_jogo WHERE tempo_id_tempo = '{$idTempo}'";
 			$resultado = $this->conexao->banco->Execute($sql);
 			$registro = $resultado->FetchNextObject();
 			$dadosTimeJogo = new TimeJogo();
-			$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME,$registro->TEMPO_ID_TEMPO);
+			$dadosTimeJogo->__constructOverload($registro->TIME_ID_TIME, $registro->TEMPO_ID_TEMPO);
 			return $dadosTimeJogo;
 	}
 
@@ -76,7 +80,7 @@ class TimeJogoDAO{
 		The method is responsibility by to delete data in database table.
 	*/
 	public function excluir($id){
-			$sql = "DELETE FROM jogo_time WHERE jogo_id_jogo= '{$id}' ";
+			$sql = "DELETE FROM jogo_time WHERE jogo_id_jogo = '{$id}' ";
 			$resultado = $this->conexao->banco->Execute($sql);
 	}
 }
