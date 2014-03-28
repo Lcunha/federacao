b271b83f4fb3279debe2b79e54f139daddc90667
caiquepereira <caiquepereira@gmail.com>
2014-03-27 22:43:07 -0300

    Added functions comments to view/JogadorView.php [style and design]

--- view/JogadorView.php
+++ view/JogadorView.php
@@ -9,36 +9,60 @@ include_once(__APP_PATH.'/controller/TimeController.php');
 class JogadorView{
 	private $jogadorCO;
         /*
-            This function is responsible to .
+            This function is responsible to instantiate the JogadorController.
         */	
 	public function __construct(){
 		$this->jogadorCO = new JogadorController();
 	}
+        /*
+            This function is responsible to save the player date received by form.
+        */
 	public function salvar(){
 		$formulario = $_POST;
 		$this->jogadorCO->_salvar($formulario['idTime'],$formulario['nome'], $formulario['data_nascimento'],$formulario['cpf'],$formulario['numero']);
 		echo "Dados inseridos com sucesso";
 	}
+        /*
+            This function is responsible to list the teams.
+        */
 	public function listarTimesParaSelect(){
 		$timeCO = new TimeController();
 		return $timeCO->_listarTimesParaSelect();
 	}
+        /*
+            This function is responsible to list the player to the table.
+        */
 	public function listarJogadoresParaTabela(){
 		return $this->jogadorCO->_listarJogadoresParaTabela();
 	}
+        /*
+            This function is responsible to list the players of team A.
+        */
 	public function listarJogadoresParaSumulaTimeA($time,$idTempo){
 		return $this->jogadorCO->_listarJogadoresParaSumulaTimeA($time,$idTempo);
 	}
+        /*
+            This function is responsible to list the player of team B.
+        */
 	public function listarJogadoresParaSumulaTimeB($time,$idTempo){
 		return $this->jogadorCO->_listarJogadoresParaSumulaTimeB($time,$idTempo);
 	}
+        /*
+            This function is responsible to update the player information.
+        */
 	public function atualizar($id){
 		$formulario = $_POST;
 		return $this->jogadorCO->_atualizar($id,$formulario['idTime'], $formulario['nome'], $formulario['data_nascimento'],$formulario['cpf'],$formulario['numero']);
 	}
+        /*
+            This function is responsible to delete the player.
+        */
 	public function excluir($id){
 		return $this->jogadorCO->_excluir($id);
 	}
+        /*
+            This function is responsible to consult player by the id.
+        */
 	public function consultarPorId($id){
 		return $this->jogadorCO->_consultarPorId($id);
 	}
