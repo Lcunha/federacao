f62b51fb76d12dc271179b1081c358562c889900
caiquepereira <caiquepereira@gmail.com>
2014-03-27 23:29:00 -0300

    Added functions comments to view/MainView.php [style and design]

--- view/JogoView.php
+++ view/JogoView.php
@@ -15,7 +15,6 @@ class JogoView {
     /*
       This function is responsible to instantiate the JogoController and TempoController.
      */
-
     public function __construct() {
         $this->jogoCO = new JogoController();
         $this->tempoCO = new TempoController();
@@ -24,7 +23,6 @@ class JogoView {
     /*
       This function is responsible to list all the games.
      */
-
     public function listarTodos() {
         return $this->jogoCO->_listarTodos();
     }
@@ -32,7 +30,6 @@ class JogoView {
     /*
       This function is responsible to consult games by the id.
      */
-
     public function consultarPorId($id) {
         return $this->jogoCO->_consultarPorId($id);
     }
@@ -40,7 +37,6 @@ class JogoView {
     /*
       This function is responsible to add a new game.
      */
-
     public function inserir() {
         return $this->jogoCO->_inserir($_POST['espectadores'], $_POST['cidade'], $_POST['local'], $_POST['data'], $_POST['hora'], $_POST['duracao'], 0);
     }
@@ -48,7 +44,6 @@ class JogoView {
     /*
       This function is responsible to save game information received by form.
      */
-
     public function salvar() {
         return $this->jogoCO->_salvar($_POST['idTimeA'], $_POST['idTimeB'], $_POST['espectadorez'], $_POST['cidade'], $_POST['local'], $_POST['data'], $_POST['hora'], $_POST['duracao']);
     }
@@ -56,7 +51,6 @@ class JogoView {
     /*
       This function is responsible to delete a game.
      */
-
     public function excluir($id) {
         return $this->jogoCO->_excluir($id);
     }
@@ -64,7 +58,6 @@ class JogoView {
     /*
       This function is responsible to add time to a game.
      */
-
     public function inserirTempo($idJogo) {
         return $this->tempoCO->_inserirTempo($idJogo);
     }
--- view/MainView.php
+++ view/MainView.php
@@ -13,31 +13,55 @@ class MainView{
 	private $jogadorCO;
 	private $tecnicoCO;
 	private $arbitroCO;
-	
+	 
+        /*
+            This function is responsible to instantiate the TimeController, JogadorController, TecnicoController and ArbitroController.
+        */        
 	public function __construct(){
 		$this->timeCO = new TimeController();
 		$this->jogadorCO = new JogadorController();
 		$this->tecnicoCO = new TecnicoController();
 		$this->arbitroCO = new ArbitroController();
 	}
+        /*
+            This function is responsible to count the team records.
+        */
 	public function contarRegistrosTime(){
 		return $this->timeCO->_contarRegistrosTime();
 	}
+        /*
+            This function is responsible to count the player records.
+        */
 	public function contarRegistrosJogador(){
 		return $this->jogadorCO->_contarRegistrosJogador();
 	}
+        /*
+            This function is responsible to count the coach records.
+        */
 	public function contarRegistrosTecnico(){
 		return $this->tecnicoCO->_contarRegistrosTecnico();
 	}
+        /*
+            This function is responsible to count the Arbitro records.
+        */
 	public function contarRegistrosArbitro(){
 		return $this->arbitroCO->_contarRegistrosArbitro();
 	}
+        /*
+            This function is responsible to list the Artilheiros.
+        */
 	public function listarArtilheiros(){
 		return $this->jogadorCO->_listarArtilheiros();
 	}
+        /*
+            This function is responsible to list the fear players.
+        */
 	public function listarFearPlayers(){
 		return $this->jogadorCO->_listarFearPlayers();
 	}
+        /*
+            This function is responsible to list teams to the championship table.
+        */
 	public function listarTimesParaTabelaCampeonato(){
 		return $this->timeCO->_listarTimesParaTabelaCampeonato();
 	}
