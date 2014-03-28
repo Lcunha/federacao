bb704fbe33dddb31a77f92adc9770ac1b32f0e66
caiquepereira <caiquepereira@gmail.com>
2014-03-27 23:52:26 -0300

    Added functions comments to view/TimeJogoView.php [style and design]

--- view/TimeJogoView.php
+++ view/TimeJogoView.php
@@ -7,11 +7,16 @@ include_once (__APP_PATH.'/controller/TimeJogoController.php');
 
 class TimeJogoView{
 	private $timeJogoCO;
-
+        /*
+            This function is responsible to instantiate the TimeJogoController.
+        */ 
 	public function __construct(){
 		$this->timeJogoCO = new TimeJogoController();
 	}
-	public function salvar($idJogoAtual,$idTime){
+	/*
+            This function is responsible to save the game.
+        */ 
+        public function salvar($idJogoAtual,$idTime){
 		$this->timeJogoCO->_salvar($idJogoAtual,$idTime);
 	}
 }
