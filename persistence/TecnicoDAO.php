8ce6a04a81a87c9a0fe7915bb960a6b0d466d7b5
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-03-28 22:38:30 -0300

    Added indentation rules in persistence/TecnicoDAO.php [Style and Design]

--- persistence/TecnicoDAO.php
+++ persistence/TecnicoDAO.php
@@ -1,9 +1,11 @@
 <?php
+
 /*
 	Name: TecnicoDAO.php  
 	Description:Class data persistence with CRUD functions (create, read, update, delete) 
 	for handling the type Coach, in the relevant table in mysql.
  */
+
 include_once (__APP_PATH.'/model/Tecnico.php');
 include_once (__APP_PATH.'/persistence/Conexao.php');
 
@@ -11,82 +13,91 @@ class TecnicoDAO{
 	private $conexao;
 	
 	/*
-		The method is responsibility to instance the class.
+            The method is responsibility to instance the class.
 	*/
 	public function __construct(){
-		$this->conexao = new Conexao();
+            $this->conexao = new Conexao();
 	}
 	
 	/*
-		The method is responsibility to list all registers in database table.
+            The method is responsibility to list all registers in database table.
 	*/
 	public function listarTodos(){
-		$sql = "SELECT * FROM tecnico";
-		$resultado = $this->conexao->banco->Execute($sql);
-		while($registro = $resultado->FetchNextObject()){
-			$dadosTecnico = new Tecnico();
-			$dadosTecnico->__constructOverload($registro->ID_TECNICO,$registro->NOME,$registro->TELEFONE,$registro->CPF);
-			$retornaTecnico[] = $dadosTecnico;
-		}
-		return $retornaTecnico;
+            $sql = "SELECT * FROM tecnico";
+            $resultado = $this->conexao->banco->Execute($sql);
+            while($registro = $resultado->FetchNextObject()){
+                    $dadosTecnico = new Tecnico();
+                    $dadosTecnico->__constructOverload($registro->ID_TECNICO, $registro->NOME,
+                                                       $registro->TELEFONE, $registro->CPF);
+                    $retornaTecnico[] = $dadosTecnico;
+            }
+            return $retornaTecnico;
 	}
 	
 	/*
-		The method is responsibility to insert data in database table.
+            The method is responsibility to insert data in database table.
 	*/
 	public function inserir(Tecnico $dadosTecnico){
-		$sql = "INSERT INTO tecnico (nome,telefone,cpf) VALUES ('{$dadosTecnico->__getNome()}','{$dadosTecnico->__getTelefone()}','{$dadosTecnico->__getCpf()}')";
-		$this->conexao->banco->Execute($sql);
+            $sql = "INSERT INTO tecnico (nome , telefone, cpf) 
+                    VALUES ('{$dadosTecnico->__getNome()}', '{$dadosTecnico->__getTelefone()}',
+                            '{$dadosTecnico->__getCpf()}')";
+            $this->conexao->banco->Execute($sql);
 	}
 	
 	/*
-		The method is responsibility to update data in database table.
+            The method is responsibility to update data in database table.
 	*/
 	public function atualizar(Tecnico $dadosTecnico){
-		$sql = "UPDATE tecnico SET nome='{$dadosTecnico->__getNome()}', telefone='{$dadosTecnico->__getTelefone()}', cpf='{$dadosTecnico->__getCpf()}' WHERE id_tecnico='{$dadosTecnico->__getIdTecnico()}' ";
-		$this->conexao->banco->Execute($sql);
-		return $dadosTecnico;
+            $sql = "UPDATE tecnico 
+                    SET nome = '{$dadosTecnico->__getNome()}', 
+                        telefone = '{$dadosTecnico->__getTelefone()}', 
+                        cpf = '{$dadosTecnico->__getCpf()}' 
+                    WHERE id_tecnico = '{$dadosTecnico->__getIdTecnico()}' ";
+            $this->conexao->banco->Execute($sql);
+            return $dadosTecnico;
 	}
 	
 	/*
-		The method is responsibility to consult in database table through of ID.
+            The method is responsibility to consult in database table through of ID.
 	*/
 	public function consultarPorId($id){
-		$sql = "SELECT * FROM tecnico WHERE id_tecnico= '{$id}'";
-		$resultado = $this->conexao->banco->Execute($sql);
-		$registro = $resultado->FetchNextObject();
-		$dadosTecnico = new Tecnico();
-		$dadosTecnico->__constructOverload($registro->ID_TECNICO,$registro->NOME,$registro->TELEFONE,$registro->CPF);
-		return $dadosTecnico;
+            $sql = "SELECT * FROM tecnico WHERE id_tecnico = '{$id}' ";
+            $resultado = $this->conexao->banco->Execute($sql);
+            $registro = $resultado->FetchNextObject();
+            $dadosTecnico = new Tecnico();
+            $dadosTecnico->__constructOverload($registro->ID_TECNICO, $registro->NOME,
+                                               $registro->TELEFONE, $registro->CPF);
+            return $dadosTecnico;
 	}
 	
 	/*
-		The method is responsibility to consult in database table through of Name.
+            The method is responsibility to consult in database table through of Name.
 	*/
 	public function consultarPorNome($nome){
-		$sql = "SELECT * FROM tecnico WHERE nome= '{$nome}'";
-		$resultado = $this->conexao->banco->Execute($sql);
-		$registro = $resultado->FetchNextObject();
-		$dadosTecnico = new Tecnico();
-		$dadosTecnico->__constructOverload($registro->ID_TECNICO,$registro->NOME,$registro->TELEFONE,$registro->CPF);
-		return $dadosTecnico;
+            $sql = "SELECT * FROM tecnico WHERE nome = '{$nome}'";
+            $resultado = $this->conexao->banco->Execute($sql);
+            $registro = $resultado->FetchNextObject();
+            $dadosTecnico = new Tecnico();
+            $dadosTecnico->__constructOverload($registro->ID_TECNICO, $registro->NOME,
+                                               $registro->TELEFONE, $registro->CPF);
+            return $dadosTecnico;
 	}
 
 	/*
-		The method is responsibility to delete data in database table.
+            The method is responsibility to delete data in database table.
 	*/
 	public function excluir($id){
-		$sql = "DELETE FROM tecnico WHERE id_tecnico= '{$id}' ";
-		$resultado = $this->conexao->banco->Execute($sql);
+            $sql = "DELETE FROM tecnico WHERE id_tecnico = '{$id}' ";
+            $resultado = $this->conexao->banco->Execute($sql);
 	}
 
 	/*
-		The method is responsibility to count registers of Coach in database table. 
+            The method is responsibility to count registers of Coach in database table. 
 	*/
 	public function contarRegistrosTecnico(){
-		$sql = "SELECT COUNT(*) as contagem FROM tecnico";
-		$resultado = $this->conexao->banco->Execute($sql);
-		$registro = $resultado->FetchNextObject();
-		return $registro->CONTAGEM;
+            $sql = "SELECT COUNT(*) as contagem FROM tecnico";
+            $resultado = $this->conexao->banco->Execute($sql);
+            $registro = $resultado->FetchNextObject();
+            return $registro->CONTAGEM;
 	}
 }
