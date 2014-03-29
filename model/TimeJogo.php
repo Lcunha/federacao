daf36f5cbd665fb543911a045f2cd95ab9d320e6
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-03-28 23:08:10 -0300

    Applyin indentation rules in model/TimeJogo.php [Style and Design]

--- model/TimeJogo.php
+++ model/TimeJogo.php
@@ -7,52 +7,50 @@
     must contains 'idJogo' and 'idTime'. 
  */
 class TimeJogo{
-	private $idJogo;
-	private $idTime;
+    private $idJogo;
+    private $idTime;
 	
 /*
     Default construct declaration. Using constructor methods is appropriate for any initialization 
     that the object may need before being used.
 */
-	public function __construct(){
-	
-	}
+    public function __construct(){
+
+    }
         
 /*
     Overload construct.
 */
-	public function __constructOverload($idJogo,$idTime){
-		$this->idJogo = $idJogo;
-		$this->idTime = $idTime;
-	}
+    public function __constructOverload($idJogo, $idTime){
+    	$this->idJogo = $idJogo;
+	$this->idTime = $idTime;
+    }
         
 /*
     Method to verify the attribute 'idJogo' of the class.
 */
-	public function __getIdJogo(){
-		return $this->idJogo;
-	}
+    public function __getIdJogo(){
+	return $this->idJogo;
+    }
         
 /*
     Method to modify the attribute 'idJogo' of the class.
 */
-	public function __setIdJogo($idJogo){
-		$this->idJogo = $idJogo;
-	}
+    public function __setIdJogo($idJogo){
+	$this->idJogo = $idJogo;
+    }
         
 /*
     Method to verify the attribute 'idTime' of the class.
 */
-	public function __getIdTime(){
-		return $this->idTime;
-	}
+    public function __getIdTime(){
+	return $this->idTime;
+    }
         
 /*
     Method to modify the attribute 'idTime' of the class.
 */
-	public function __setIdTime($idTime){
-		$this->idTime = $idTime;
-	}
-	
-	
+    public function __setIdTime($idTime){
+	$this->idTime = $idTime;
+    }
 }
