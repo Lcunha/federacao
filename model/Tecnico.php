2bbedf3849879b3dd70b47b1bc0d1019a7eb57a7
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-03-28 22:35:26 -0300

    Applying indentation rules in model/Tecnico.php [Style and Design]

--- model/Tecnico.php
+++ model/Tecnico.php
@@ -6,87 +6,83 @@
     and constructors of a coach. The coach must contains 'idTecnico', 'nome',
     'telefone' and 'cpf'. 
  */
-class Tecnico
-{
-	private $idTecnico;
-	private $nome;
-	private $telefone;
-	private $cpf;
+class Tecnico{
+    private $idTecnico;
+    private $nome;
+    private $telefone;
+    private $cpf;
 	
 /*
     Default construct declaration. Using constructor methods is appropriate for any initialization 
     that the object may need before being used.
 */
-	public function __construct(){
+    public function __construct(){
 		
-	}
+    }
         
 /*
     Overload construct.
 */
-	public function __constructOverload($idTecnico,$nome,$telefone,$cpf){
-		$this->idTecnico = $idTecnico;
-		$this->nome = $nome;
-		$this->telefone = $telefone;
-		$this->cpf = $cpf;
-	}
+    public function __constructOverload($idTecnico, $nome, $telefone, $cpf){
+    	$this->idTecnico = $idTecnico;
+	$this->nome = $nome;
+	$this->telefone = $telefone;
+	$this->cpf = $cpf;
+    }
         
 /*
     Method to verify the attribute 'idTecnico' of the class.
 */
-	public function __getIdTecnico(){
-		return $this->idTecnico;
-	}
+    public function __getIdTecnico(){
+	return $this->idTecnico;
+    }
         
 /*
     Method to modify the attribute 'idTecnico' of the class.
 */
-	public function __setIdTecnico($idTecnico){
-		$this->idTecnico = $idTecnico;
-	}
+    public function __setIdTecnico($idTecnico){
+	$this->idTecnico = $idTecnico;
+    }
         
 /*
     Method to verify the attribute 'nome' of the class.
 */
-	public function __getNome(){
-		return $this->nome;
-	}
+    public function __getNome(){
+	return $this->nome;
+    }
         
 /*
     Method to modify the attribute 'nome' of the class.
 */
-	public function __setNome($nome){
-		$this->nome = $nome;
-	}
+    public function __setNome($nome){
+	$this->nome = $nome;
+    }
         
 /*
     Method to verify the attribute 'telefone' of the class.
 */
-	public function __getTelefone(){
-		return $this->telefone;
-	}
+    public function __getTelefone(){
+	return $this->telefone;
+    }
         
 /*
     Method to modify the attribute 'telefone' of the class.
 */
-	public function __setTelefone($telefone){
-		$this->telefone = $telefone;
-	}
+    public function __setTelefone($telefone){
+	$this->telefone = $telefone;
+    }
         
 /*
     Method to verify the attribute 'cpf' of the class.
 */
-	public function __getCpf(){
-		return $this->cpf;
-	}
+    public function __getCpf(){
+	return $this->cpf;
+    }
         
 /*
     Method to modify the attribute 'cpf' of the class.
 */
-	public function __setCpf($cpf){
-		$this->cpf = $cpf;
-	}
+    public function __setCpf($cpf){
+	$this->cpf = $cpf;
+    }
 }
-
-
-
