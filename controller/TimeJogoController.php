3a6d760cb882e9201c2180b2873b932d2f10494b
Larissa <larissax55@gmail.com>
2014-03-28 23:22:17 -0300

    Correction [style and desing]

--- controller/TimeJogoController.php
+++ controller/TimeJogoController.php
@@ -1,11 +1,10 @@
 <?php
 
 /*
-  Name: TimeJogoController.php
-  Description: Class that performs the listing of teams and
-  games already registered in the system in a table may exluir, edit, update and save.
+    Name: TimeJogoController.php
+    Description: Class that performs the listing of teams and
+    games already registered in the system in a table may exluir, edit, update and save.
  */
-
 include_once(__APP_PATH . '/persistence/TimeJogoDAO.php');
 include_once(__APP_PATH . '/model/TimeJogo.php');
 
@@ -18,13 +17,12 @@ class TimeJogoController {
     }
 
     /*
-      Function we list all data of the games that are being played on the table grabbing data from teams in the game.
-     */
-
-    public function _listarTimeJogoParaTabela() {
+        Function we list all data of the games that are being played on the table grabbing data from teams in the game.
+    */
+    public function _listarTimeJogoParaTabela(){
         $dadosTimeJogo = new TimeJogo();
         $arrayDadosTimeJogo = $this->timeJogoDAO->listarTodos();
-        for ($i = 0; $i < count($arrayDadosTimeJogo); $i++) {
+        for ($i = 0; $i < count($arrayDadosTimeJogo); $i++){
             $dadosTimeJogo = $arrayDadosTimeJogo[$i];
             $arrayTr[] = "
 			<tr>
@@ -43,62 +41,56 @@ class TimeJogoController {
     }
 
     /*
-      Function responsible for listing all games
+        Function responsible for listing all games.
      */
-
-    public function _listarTodos() {
+    public function _listarTodos(){
         return $this->timeJogoDAO->listarTodos();
     }
 
     /*
-      Function responsible for performing a query by team.
-     */
-
-    public function _consultarPorTime($idTime) {
+        Function responsible for performing a query by team.
+    */
+    public function _consultarPorTime($idTime){
         return $this->timeJogoDAO->consultarPorTime($idTime);
     }
 
     /*
-      Function responsible for performing a query by time.
-     */
-
-    public function _consultarPorTempo($idTempo) {
+        Function responsible for performing a query by time.
+    */
+    public function _consultarPorTempo($idTempo){
         return $this->timeJogoDAO->consultarPorTempo($idTempo);
     }
 
     /*
-      Function responsible for performing a query by game.
-     */
-
-    public function _inserirTimeJogo(TimeJogo $timeJogo) {
+        Function responsible for performing a query by game.
+    */
+    public function _inserirTimeJogo(TimeJogo $timeJogo){
         return $this->timeJogoDAO->inserir($timeJogo);
     }
 
     /*
-      Function responsible for updating the game data.
-     */
+        Function responsible for updating the game data.
+    */
 
-    public function _atualizar($idJogo, $idTime) {
+    public function _atualizar($idJogo, $idTime){
         $dadosTimeJogo = new TimeJogo();
         $dadosTimeJogo->__constructOverload($idJogo, $idTime);
         $this->timeJogoDAO->atualizar($dadosTimeJogo);
     }
 
     /*
-      Function salver responsible for the data of the games that are being played.
-     */
-
-    public function _salvar($idJogoAtual, $idTime) {
+        Function salver responsible for the data of the games that are being played.
+    */
+    public function _salvar($idJogoAtual, $idTime){
         $dadosTimeJogo = new TimeJogo();
         $dadosTimeJogo->__constructOverload($idJogoAtual, $idTime);
         $this->timeJogoDAO->inserir($dadosTimeJogo);
     }
 
     /*
-      Function responsible for deleting the data on team playing.
-     */
-
-    public function _excluir($id) {
+        Function responsible for deleting the data on team playing.
+    */
+    public function _excluir($id){
         return $this->timeJogoDAO->excluir($id);
     }
 
