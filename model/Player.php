2b02c70481c3b05b6d93a53b5aa7bfa634b048ba
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-04-25 14:40:34 -0300

    Changing class name of model/Jogador.php for model/Player.php

---
+++ model/Player.php
@@ -0,0 +1,122 @@
+<?php
+
+/* 
+    Name: Player.php
+    Description: This is the class that contains the attributes, accessor methods 
+    and constructors of a player. The player must contains 'idPlayer', 'idTeam', 
+    'playerName', 'birthDatePlayer', 'cpf' and 'numberPlayer'. 
+*/
+class Player{
+    private $idPlayer;
+    private $idTeam;
+    private $playerName;
+    private $birthDatePlayer;
+    private $cpf;
+    private $numberPlayer;
+	
+    /*
+        Default construct declaration. Using constructor methods is appropriate for any initialization 
+        that the object may need before being used.
+    */
+    public function __construct(){
+		
+    }
+        
+    /*
+        Overload construct.
+    */
+    public function __constructOverload($idPlayer, $idTeam, $playerName, $birthDatePlayer, $cpf, 
+                                        $numberPlayer){
+    	$this->idPlayer = $idPlayer;
+	$this->idTeam = $idTeam;
+	$this->playerName = $playerName;
+	$this->birthDatePlayer = $birthDatePlayer;
+	$this->cpf = $cpf;
+	$this->numberPlayer = $numberPlayer;
+    }
+        
+    /*
+        Method to verify the attribute 'idPlayer' of the class.
+    */
+    public function __getIdPlayer(){
+	return $this->idPlayer;
+    }
+        
+    /*
+        Method to modify the attribute 'idPlayer' of the class.
+    */
+    public function __setIdPlayer($idPlayer){
+	$this->idPlayer = $idPlayer;
+    }
+        
+    /*
+        Method to verify the attribute 'playerName' of the class.
+    */
+    public function __getPlayerName(){
+	return $this->playerName;
+    }
+        
+    /*
+        Method to modify the attribute 'playerName' of the class.
+    */
+    public function __setPlayerName($playerName){
+	$this->playerName = $playerName;
+    }
+        
+    /*
+        Method to verify the attribute 'birthDatePlayer' of the class.
+    */
+    public function __getBirthDatePlayer(){
+	return $this->birthDatePlayer;
+    }
+        
+    /*
+        Method to modify the attribute 'birthDatePlayer' of the class.
+    */
+    public function __setBirthDatePlayer($birthDatePlayer){
+	$this->birthDatePlayer = $birthDatePlayer;
+    }
+        
+    /*
+        Method to verify the attribute 'cpf' of the class.
+    */
+    public function __getCpf(){
+	return $this->cpf;
+    }
+        
+    /*
+        Method to modify the attribute 'cpf' of the class.
+    */
+    public function __setCpf($cpf){
+	$this->cpf = $cpf;
+    }
+        
+    /*
+        Method to verify the attribute 'idTeam' of the class.
+    */
+    public function __getIdTeam(){
+	return $this->idTeam;
+    }
+        
+    /*
+        Method to modify the attribute 'idTeam' of the class.
+    */
+    public function __setIdTeam($idTeam){
+	$this->idTeam = $idTeam;
+    }
+        
+    /*
+        Method to verify the attribute 'numberPlayer' of the class.
+    */
+    public function __getNumberPlayer(){
+	return $this->numberPlayer;
+    }
+        
+    /*
+        Method to modify the attribute 'numberPlayer' of the class.
+    */
+    public function __setNumberPlayer($numberPlayer){
+	$this->numberPlayer = $numberPlayer;
+    }
+}
+
