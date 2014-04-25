45690bff63566d1dc7f0dc24dcca7aeaec270d6d
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-04-25 18:25:30 -0300

    Changing variables,functions and classes names in view/Player.php

---
+++ view/PlayerView.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+    Name: PlayerView.php
+    Description: This view shows the options of the players.
+*/
+include_once(__APP_PATH.'/controller/PlayerController.php');
+include_once(__APP_PATH.'/controller/TeamController.php');
+
+class PlayerView{
+	private $playerCO;
+        /*
+            This function is responsible to instantiate the PlayerController.
+        */	
+	public function __construct(){
+		$this->playerCO = new PlayerController();
+	}
+        /*
+            This function is responsible to save the player date received by 
+            form.
+        */
+	public function savePlayer(){
+		$formPlayer = $_POST;
+		$this->playerCO->_savePlayer($formPlayer['IdTeam'], 
+                $formPlayer['playerName'], $formPlayer['data_nascimento'], 
+                $formPlayer['cpf'], $formPlayer['playerNumber']);
+		echo "Dados inseridos com sucesso";
+	}
+        /*
+            This function is responsible to list the teams.
+        */
+	public function listAllTeamsForSelect(){
+		$teamCO = new TeamController();
+		return $teamCO->_listTeamsParaSelect();
+	}
+        /*
+            This function is responsible to list the player to the table.
+        */
+	public function listAllPlayersForTables(){
+		return $this->playerCO->_listAllPlayersForTables();
+	}
+        /*
+            This function is responsible to list the players of team A.
+        */
+	public function listAllPlayersForDocketTeamA($team, $idTimePlay){
+		return $this->playerCO->_listAllPlayersForDocketTeamA($team, 
+                $idTimePlay);
+	}
+        /*
+            This function is responsible to list the player of team B.
+        */
+	public function listAllPlayersForDocketTeamB($team, $idTimePlay){
+		return $this->playerCO->_listAllPlayersForDocketTeamB($team, 
+                $idTimePlay);
+	}
+        /*
+            This function is responsible to update the player information.
+        */
+	public function updatePlayer($idPlayer){
+		$formPlayer = $_POST;
+		return $this->playerCO->_atualizar($idPlayer, $formPlayer['IdTeam'],  
+                $formPlayer['playerName'], $formPlayer['data_nascimento'], 
+                $formPlayer['cpf'], $formPlayer['playerNumber']);
+	}
+        /*
+            This function is responsible to delete the player.
+        */
+	public function deletePlayer($idPlayer){
+		return $this->playerCO->_deletePlayer($idPlayer);
+	}
+        /*
+            This function is responsible to consult player by the id.
+        */
+	public function consultByIdPlayer($idPlayer){
+		return $this->playerCO->_consultByIdPlayer($idPlayer);
+	}
+}
