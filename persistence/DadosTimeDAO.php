0de279dd9265ead1acf5881053973e5f21f6de43
Larissa <larissax55@gmail.com>
2014-04-26 00:25:10 -0300

    changing the name of the variables in the class DadosTimeView.php

--- persistence/DadosTimeDAO.php
+++ persistence/DadosTimeDAO.php
@@ -26,36 +26,36 @@ class DadosTimeDAO{
 	*/
 	public function listarTodos(){
             $sql = "SELECT * FROM dados_campeonato";
-            $resultado = $this->conexao->banco->Execute($sql);
-            while($registro = $resultado->FetchNextObject()){
-                    $dadosDadosTime = new DadosTime();
-                    $dadosDadosTime->__constructOverload($registro->ID_DADOS_CAMPEONATO,
-                                                         $registro->TIME_ID_TIME,
-                                                         $registro->PONTOS, $registro->JOGOS,
-                                                         $registro->VITORIAS,
-                                                         $registro->EMPATES,
-                                                         $registro->DERROTAS,
-                                                         $registro->GOLS,
-                                                         $registro->GOLS_LEVADOS);
-                    $retornaDadosTime[] = $dadosDadosTime;
+            $result = $this->conexao->banco->Execute($sql);
+            while($record = $result->FetchNextObject()){
+                    $dataDataTeam = new DadosTime();
+                    $dataDataTeam->__constructOverload($record->ID_DADOS_CAMPEONATO,
+                                                         $record->TIME_ID_TIME,
+                                                         $record->PONTOS, $record->JOGOS,
+                                                         $record->VITORIAS,
+                                                         $record->EMPATES,
+                                                         $record->DERROTAS,
+                                                         $record->GOLS,
+                                                         $record->GOLS_LEVADOS);
+                    $returnDataTeam[] = $dataDataTeam;
             }
-            return $retornaDadosTime;
+            return $returnDataTeam;
 	}
 
 	/*
             The method is responsibility to insert data in database table.
 	*/
-	public function inserir(DadosTime $dadosDadosTime){
+	public function inserir(DadosTime $dataDataTeam){
             $sql = "INSERT INTO dados_campeonato(pontos, jogos, vitorias, empates,
                                                  derrotas, gols, gols_levados, time_id_time)
-                    VALUES ('{$dadosDadosTime->__getPontos()}', 
-                            '{$dadosDadosTime->__getJogos()}',
-                            '{$dadosDadosTime->__getVitorias()}',
-                            '{$dadosDadosTime->__getEmpates()}',
-                            '{$dadosDadosTime->__getDerrotas()}',
-                            '{$dadosDadosTime->__getGols()}',
-                            '{$dadosDadosTime->__getGolsLevados()}',
-                            '{$dadosDadosTime->__getIdTime()}')";
+                    VALUES ('{$dataDataTeam->__getPontos()}', 
+                            '{$dataDataTeam->__getJogos()}',
+                            '{$dataDataTeam->__getVitorias()}',
+                            '{$dataDataTeam->__getEmpates()}',
+                            '{$dataDataTeam->__getDerrotas()}',
+                            '{$dataDataTeam->__getGols()}',
+                            '{$dataDataTeam->__getGolsLevados()}',
+                            '{$dataDataTeam->__getIdTime()}')";
             return $this->conexao->banco->Execute($sql);
 	}
 
@@ -64,16 +64,16 @@ class DadosTimeDAO{
 	*/
 	public function consultarPorId($id){
             $sql = "SELECT * FROM dados_campeonato WHERE id_dados_campeonato = '{$id}' ";
-            $resultado = $this->conexao->banco->Execute($sql);
-            $registro = $resultado->FetchNextObject();
-            $dadosDadosTime = new DadosTime();
-            $dadosDadosTime->__constructOverload($registro->ID_DADOS_CAMPEONATO, 
-                                                 $registro->TIME_ID_TIME,
-                                                 $registro->PONTOS,
-                                                 $registro->JOGOS, $registro->VITORIAS,
-                                                 $registro->EMPATES, $registro->DERROTAS,
-                                                 $registro->GOLS,$registro->GOLS_LEVADOS);
-            return $dadosDadosTime;
+            $result = $this->conexao->banco->Execute($sql);
+            $record = $result->FetchNextObject();
+            $dataDataTeam = new DadosTime();
+            $dataDataTeam->__constructOverload($record->ID_DADOS_CAMPEONATO, 
+                                                 $record->TIME_ID_TIME,
+                                                 $record->PONTOS,
+                                                 $record->JOGOS, $record->VITORIAS,
+                                                 $record->EMPATES, $record->DERROTAS,
+                                                 $record->GOLS,$record->GOLS_LEVADOS);
+            return $dataDataTeam;
 	}
 
 	/*
@@ -81,30 +81,30 @@ class DadosTimeDAO{
 	*/
 	public function consultarPorIdTime($id){
             $sql = "SELECT * FROM dados_campeonato WHERE time_id_time = '{$id}' ";
-            $resultado = $this->conexao->banco->Execute($sql);
-            $registro = $resultado->FetchNextObject();
-            $dadosDadosTime = new DadosTime();
-            $dadosDadosTime->__constructOverload($registro->ID_DADOS_CAMPEONATO,
-                                                 $registro->TIME_ID_TIME, $registro->PONTOS,
-                                                 $registro->JOGOS, $registro->VITORIAS,
-                                                 $registro->EMPATES, $registro->DERROTAS,
-                                                 $registro->GOLS, $registro->GOLS_LEVADOS);
-            return $dadosDadosTime;
+            $result = $this->conexao->banco->Execute($sql);
+            $record = $result->FetchNextObject();
+            $dataDataTeam = new DadosTime();
+            $dataDataTeam->__constructOverload($record->ID_DADOS_CAMPEONATO,
+                                                 $record->TIME_ID_TIME, $record->PONTOS,
+                                                 $record->JOGOS, $record->VITORIAS,
+                                                 $record->EMPATES, $record->DERROTAS,
+                                                 $record->GOLS, $record->GOLS_LEVADOS);
+            return $dataDataTeam;
 	}
 
 	/*
             The method is responsibility to update data in database table.
 	*/
-	public function atualizar(DadosTime $dadosDadosTime){
+	public function atualizar(DadosTime $dataDataTeam){
             $sql = "UPDATE dados_campeonato 
-                    SET pontos = '{$dadosDadosTime->__getPontos()}', 
-                        jogos = '{$dadosDadosTime->__getJogos()}', 
-                        vitorias = '{$dadosDadosTime->__getVitorias()}', 
-                        empates = '{$dadosDadosTime->__getEmpates()}', 
-                        derrotas = '{$dadosDadosTime->__getDerrotas()}', 
-                        gols = '{$dadosDadosTime->__getGols()}', 
-                        gols_levados = '{$dadosDadosTime->__getGolsLevados()}' 
-                    WHERE time_id_time = '{$dadosDadosTime->__getIdTime()}' ";
+                    SET pontos = '{$dataDataTeam->__getPontos()}', 
+                        jogos = '{$dataDataTeam->__getJogos()}', 
+                        vitorias = '{$dataDataTeam->__getVitorias()}', 
+                        empates = '{$dataDataTeam->__getEmpates()}', 
+                        derrotas = '{$dataDataTeam->__getDerrotas()}', 
+                        gols = '{$dataDataTeam->__getGols()}', 
+                        gols_levados = '{$dataDataTeam->__getGolsLevados()}' 
+                    WHERE time_id_time = '{$dataDataTeam->__getIdTime()}' ";
             $this->conexao->banco->Execute($sql);
 	}
 
@@ -112,76 +112,76 @@ class DadosTimeDAO{
 		The method is responsibility by to update of points and information data in 
                 database table.
 	*/
-	public function atualizarPontos($idTimeA, $idTimeB, $pontosTimeA, $pontosTimeB, $golsTimeA,
-                                        $golsTimeB){
-            $timeA = new TimeDAO();
-            $timeA = $this->consultarPorIdTime($idTimeA);
-            $timeB = $this->consultarPorIdTime($idTimeB);
-            $novoJogoA = $timeA->__getJogos()+1;
-            $novoJogoB = $timeB->__getJogos()+1;
-            if($golsTimeA > $golsTimeB){
-                    $novaVitoriaA = $timeA->__getVitorias() + 1;
-                    $novaDerrotaB = $timeB->__getDerrotas() + 1;
-                    $novoGolsA = $timeA->__getGols() + $golsTimeA;
-                    $novoGolsB = $timeB->__getGols() + $golsTimeB;
-                    $novoGolsLevadosA = $timeA->__getGolsLevados() + $golsTimeB;
-                    $novoGolsLevadosB = $timeB->__getGolsLevados() + $golsTimeA;
-                    $novoPontosA = $timeA->__getPontos() + $pontosTimeA;
-                    $novoPontosB = $timeB->__getPontos() + $pontosTimeB;
+	public function atualizarPontos($idTeam1, $idTeam2, $pointsTeam1, $pointsTeam2, $goalsTeam1,
+                                        $goalsTeam2){
+            $team1 = new TimeDAO();
+            $team1 = $this->consultarPorIdTime($idTeam1);
+            $team2 = $this->consultarPorIdTime($idTeam2);
+            $newPlayer1 = $team1->__getJogos()+1;
+            $newPlayer2 = $team2->__getJogos()+1;
+            if($goalsTeam1 > $goalsTeam2){
+                    $newVictory1= $team1->__getVitorias() + 1;
+                    $newDefeat2 = $team2->__getDerrotas() + 1;
+                    $newGoals1 = $team1->__getGols() + $goalsTeam1;
+                    $newGoals2 = $team2->__getGols() + $goalsTeam2;
+                    $newGoalsTaken1 = $team1->__getGolsLevados() + $goalsTeam2;
+                    $newGoalsTaken2 = $team2->__getGolsLevados() + $goalsTeam1;
+                    $newPoints1 = $team1->__getPontos() + $pointsTeam1;
+                    $newPoints2 = $team2->__getPontos() + $pointsTeam2;
                     $sqlA = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosA}', 
-                                 jogos = '{$novoJogoA}', 
+                             SET pontos = '{$newPoints1}', 
+                                 jogos = '{$newPlayer1}', 
                                  vitorias = '{$novaVitoriaA}',
-                                 gols = '{$novoGolsA}', 
-                                 gols_levados = '{$novoGolsLevadosA}' 
-                             WHERE time_id_time = '{$timeA->__getIdTime()}' ";
+                                 gols = '{$newGoals1}', 
+                                 gols_levados = '{$newGoalsTaken1}' 
+                             WHERE time_id_time = '{$team1->__getIdTime()}' ";
                     $sqlB = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosB}', 
-                                 jogos = '{$novoJogoB}', 
-                                 derrotas = '{$novaDerrotaB}',
-                                 gols = '{$novoGolsB}', 
-                                 gols_levados = '{$novoGolsLevadosB}' 
-                             WHERE time_id_time = '{$timeB->__getIdTime()}' ";
+                             SET pontos = '{$newPoints2}', 
+                                 jogos = '{$newPlayer2}', 
+                                 derrotas = '{$newDefeat2}',
+                                 gols = '{$newGoals2}', 
+                                 gols_levados = '{$newGoalsTaken2}' 
+                             WHERE time_id_time = '{$team2->__getIdTime()}' ";
             }
-            else if($golsTimeA < $golsTimeB){
-                    $novaVitoriaB = $timeB->__getVitorias() + 1;
-                    $novaDerrotaA = $timeA->__getDerrotas() + 1;
-                    $novoGolsA = $timeA->__getGols() + $golsTimeA;
-                    $novoGolsB = $timeB->__getGols() + $golsTimeB;
-                    $novoGolsLevadosA = $timeA->__getGolsLevados() + $golsTimeB;
-                    $novoGolsLevadosB = $timeB->__getGolsLevados() + $golsTimeA;
-                    $novoPontosA = $timeA->__getPontos() + $pontosTimeA;
-                    $novoPontosB = $timeB->__getPontos() + $pontosTimeB;
+            else if($goalsTeam1 < $goalsTeam2){
+                    $novaVitoriaB = $team2->__getVitorias() + 1;
+                    $novaDerrotaA = $team1->__getDerrotas() + 1;
+                    $newGoals1 = $team1->__getGols() + $goalsTeam1;
+                    $newGoals2 = $team2->__getGols() + $goalsTeam2;
+                    $newGoalsTaken1 = $team1->__getGolsLevados() + $goalsTeam2;
+                    $newGoalsTaken2 = $team2->__getGolsLevados() + $goalsTeam1;
+                    $newPoints1 = $team1->__getPontos() + $pointsTeam1;
+                    $newPoints2 = $team2->__getPontos() + $pointsTeam2;
                     $sqlA = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosA}', jogos = '{$novoJogoA}', 
-                                 derrotas='{$novaDerrotaA}', gols = '{$novoGolsA}', 
-                                 gols_levados = '{$novoGolsLevadosA}' 
-                             WHERE time_id_time = '{$timeA->__getIdTime()}' ";
+                             SET pontos = '{$newPoints1}', jogos = '{$newPlayer1}', 
+                                 derrotas='{$novaDerrotaA}', gols = '{$newGoals1}', 
+                                 gols_levados = '{$newGoalsTaken1}' 
+                             WHERE time_id_time = '{$team1->__getIdTime()}' ";
                     $sqlB = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosB}', jogos = '{$novoJogoB}', 
-                                 vitorias = '{$novaVitoriaB}', gols = '{$novoGolsB}', 
-                                 gols_levados = '{$novoGolsLevadosB}' 
-                             WHERE time_id_time='{$timeB->__getIdTime()}' ";
+                             SET pontos = '{$newPoints2}', jogos = '{$newPlayer2}', 
+                                 vitorias = '{$novaVitoriaB}', gols = '{$newGoals2}', 
+                                 gols_levados = '{$newGoalsTaken2}' 
+                             WHERE time_id_time='{$team2->__getIdTime()}' ";
             }
-            else if($golsTimeA == $golsTimeB){
-                    $novoEmpateA = $timeA->__getEmpates() + 1;
-                    $novoEmpateB = $timeB->__getEmpates() + 1;
-                    $novoGolsA = $timeA->__getGols() + $golsTimeA;
-                    $novoGolsB = $timeB->__getGols() + $golsTimeB;
-                    $novoGolsLevadosA = $timeA->__getGolsLevados() + $golsTimeB;
-                    $novoGolsLevadosB = $timeB->__getGolsLevados() + $golsTimeA;
-                    $novoPontosA = $timeA->__getPontos() + $pontosTimeA;
-                    $novoPontosB = $timeB->__getPontos() + $pontosTimeB;
+            else if($goalsTeam1 == $goalsTeam2){
+                    $newDraws1 = $team1->__getEmpates() + 1;
+                    $newDraws2 = $team2->__getEmpates() + 1;
+                    $newGoals1 = $team1->__getGols() + $goalsTeam1;
+                    $newGoals2 = $team2->__getGols() + $goalsTeam2;
+                    $newGoalsTaken1 = $team1->__getGolsLevados() + $goalsTeam2;
+                    $newGoalsTaken2 = $team2->__getGolsLevados() + $goalsTeam1;
+                    $newPoints1 = $team1->__getPontos() + $pointsTeam1;
+                    $newPoints2 = $team2->__getPontos() + $pointsTeam2;
                     $sqlA = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosA}', 
-                                 jogos = '{$novoJogoA}', empates = '{$novoEmpateA}',
-                                 gols = '{$novoGolsA}', gols_levados = '{$novoGolsLevadosA}' 
-                             WHERE time_id_time = '{$timeA->__getIdTime()}' ";
+                             SET pontos = '{$newPoints1}', 
+                                 jogos = '{$newPlayer1}', empates = '{$newDraws1}',
+                                 gols = '{$newGoals1}', gols_levados = '{$newGoalsTaken1}' 
+                             WHERE time_id_time = '{$team1->__getIdTime()}' ";
                     $sqlB = "UPDATE dados_campeonato 
-                             SET pontos = '{$novoPontosB}', jogos = '{$novoJogoB}',
-                                 empates = '{$novoEmpateB}', gols = '{$novoGolsB}',
-                                 gols_levados = '{$novoGolsLevadosB}' 
-                             WHERE time_id_time='{$timeB->__getIdTime()}' ";
+                             SET pontos = '{$newPoints2}', jogos = '{$newPlayer2}',
+                                 empates = '{$newDraws2}', gols = '{$newGoals2}',
+                                 gols_levados = '{$newGoalsTaken2}' 
+                             WHERE time_id_time='{$team2->__getIdTime()}' ";
             }
             $this->conexao->banco->Execute($sqlA);
             $this->conexao->banco->Execute($sqlB);
@@ -192,6 +192,6 @@ class DadosTimeDAO{
 	*/
 	public function excluir($id){
             $sql = "DELETE FROM dados_campeonato WHERE id_dados_campeonato = '{$id}' ";
-            $resultado = $this->conexao->banco->Execute($sql);
+            $result = $this->conexao->banco->Execute($sql);
 	}
 }
