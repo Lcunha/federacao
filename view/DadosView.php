96e43770a99f6262195c3053fb68d240ccb84fd6
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-04-26 15:16:53 -0300

    Changing the name of variables, functions and methods to english in view/DadosView.php

--- view/DadosView.php
+++ view/DadosView.php
@@ -1,45 +1,48 @@
 <?php
 
 /*
-    Name: DadosView.php
+    Name: GameDataView.php
     Description: This view shows the data added by the user.
 */
-include_once(__APP_PATH.'/controller/DadosController.php');
+include_once(__APP_PATH.'/controller/GameDataController.php');
 
-class DadosView{
-	private $dadosCO;
-        /*
-            This function is responsible to instantiate the DadosController.
-        */
-	public function __construct(){
-		$this->dadosCO = new DadosController();
-	}
-        /*
-            This function is responsible to consult the controller data.
-        */
-	public function consultarPorId($id){
-		return $this->dadosCO->_consultarPorId($id);
-	}
-        /*
-            This function is responsible to add to the controller data.
-        */
-	public function inserir($idTempoInserido, $idTimeA, $idTimeB){
-		return $this->dadosCO->_inserir($idTempoInserido,$idTimeA, 
-                $idTimeB);
-	}
-        /*
-            This function is responsible to update the controller data from the 
-            text received by the form.
-        */
-	public function atualizar($id){
-		$formulario = $_POST;
-		return $this->dadosCO->_atualizar();
-	}
-        /*
-            This function is responsible to delete the controller data.
-        */
-	public function excluir($id){
-		return $this->dadosCO->_excluir($id);
-	}
-	
+class GameDataView{
+    private $gameDataCO;
+    
+    /*
+        This function is responsible to instantiate the GameDataController.
+    */
+    public function __construct(){
+        $this->gameDataCO = new GameDataController();
+    }
+    
+    /*
+        This function is responsible to consult the controller data.
+    */
+    public function consultByIdGameData($id){
+	return $this->gameDataCO->_consultByIdGameData($id);
+    }
+    
+    /*
+        This function is responsible to add to the controller data.
+    */
+    public function insertDataTeam($idTimeInserted, $idTeam1, $idTeam2){
+	return $this->gameDataCO->_insertDataTeam($idTimeInserted,$idTeam1, $idTeam2);
+    }
+    
+    /*
+        This function is responsible to update the controller data from the text received 
+        by the form.
+    */
+    public function updateData($id){
+	$formGameData = $_POST;
+	return $this->gameDataCO->_updateData();
+    }
+    
+    /*
+        This function is responsible to delete the controller data.
+    */
+    public function deleteData($id){
+	return $this->gameDataCO->_deleteData($id);
+    }	
 }
