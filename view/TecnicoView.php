80df57eca479d98f056346c848946282a1b2392f
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-04-27 01:58:28 -0300

    Changing the name of variables, functions and methods to english in view/TecnicoView.php

--- view/TecnicoView.php
+++ view/TecnicoView.php
@@ -1,52 +1,58 @@
 <?php
 
 /*
-    Name: TecnicoView.php
+    Name: CoachView.php
     Description: This view shows the data of the coach. It can also be used to change or delete it.
 */
-include_once (__APP_PATH.'/controller/TecnicoController.php');
+include_once (__APP_PATH.'/controller/CoachController.php');
 
-class TecnicoView{
-	private $tecnicoCO;
-        /*
-            This function is responsible to instantiate the TecnicoController.
-        */ 
-	public function __construct(){
-		$this->tecnicoCO = new TecnicoController();
-	}
-        /*
-            This function is responsible to save the coach data from the forms.
-        */ 
-	public function salvar(){
-		$formulario = $_POST;
-		$this->tecnicoCO->_salvar($formulario['nome'], 
-                $formulario['telefone'], $formulario['cpf']);
-		echo "Dados inseridos com sucesso";
-	}
-        /*
-            This function is responsible to update de coach data from the forms.
-        */ 
-	public function atualizar($id){
-		$formulario = $_POST;
-		return $this->tecnicoCO->_atualizar($id,$formulario['nome'], 
-                $formulario['telefone'], $formulario['cpf']);
-	}
-        /*
-            This function is responsible to delete the coach.
-        */ 
-	public function excluir($id){
-		return $this->tecnicoCO->_excluir($id);
-	}
-        /*
-            This function is responsible to list the coachs to the table.
-        */ 
-	public function listarTecnicosParaTabela(){
-		return $this->tecnicoCO->_listarTecnicosParaTabela();
-	}
-        /*
-            This function is responsible to consult coachs by id.
-        */ 
-	public function consultarPorId($id){
-		return $this->tecnicoCO->_consultarPorId($id);
-	}
+class CoachView{
+    private $coachCO;
+    
+    /*
+        This function is responsible to instantiate the CoachController.
+    */ 
+    public function __construct(){
+	$this->coachCO = new CoachController();
+    }
+    
+    /*
+        This function is responsible to save the coach data from the forms.
+    */ 
+    public function saveCoach(){
+	$formCoach = $_POST;
+	$this->coachCO->_saveCoach($formCoach['coachName'], $formCoach['coachPhone'], 
+                                   $formCoach['coachCpf']);
+        echo "Dados inseridos com sucesso";
+    }
+    
+    /*
+        This function is responsible to update de coach data from the forms.
+    */ 
+    public function updateCoach($id){
+        $formCoach = $_POST;
+	return $this->coachCO->_updateCoach($id, $formCoach['coachName'], $formCoach['coachPhone'], 
+               $formCoach['coachCpf']);
+    }
+    
+    /*
+        This function is responsible to delete the coach.
+    */ 
+    public function deleteCoach($id){
+	return $this->coachCO->_deleteCoach($id);
+    }
+    
+    /*
+        This function is responsible to list the coachs to the table.
+    */ 
+    public function listAllCoachesForTable(){
+	return $this->coachCO->_listAllCoachesForTable();
+    }
+    
+    /*
+        This function is responsible to consult coachs by id.
+    */ 
+    public function consultByIdCoach($id){
+	return $this->coachCO->_consultByIdCoach($id);
+    }
 }
