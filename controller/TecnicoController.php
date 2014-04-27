ddf8bd5bebcaf21c94e5ca559cf41d3377de793d
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-04-27 01:41:46 -0300

    Changing the name of variables, functions and methods to english in controller/TecnicoController.php

--- controller/TecnicoController.php
+++ controller/TecnicoController.php
@@ -1,31 +1,31 @@
 <?php
 
 /*
-    Name: TecnicoController.php
+    Name: CoachController.php
     Description: Class responsible for inserting, deleting,
     changing a manager in addition to making an appointment for a technician already registered.
- */
-include_once(__APP_PATH . '/persistence/TecnicoDAO.php');
-include_once(__APP_PATH . '/model/Tecnico.php');
+*/
+include_once(__APP_PATH . '/persistence/CoachDAO.php');
+include_once(__APP_PATH . '/model/Coach.php');
 
-class TecnicoController {
+class CoachController {
 
-    private $tecnicoDAO;
+    private $coachDAO;
 
     public function __construct(){
-        $this->tecnicoDAO = new TecnicoDAO();
+        $this->coachDAO = new CoachDAO();
     }
 
     /*
         Function responsible for selecting the technical data in the table.
     */
-    public function _listarTecnicosParaSelect(){
-        $dadosTecnico = new Tecnico();
-        $arrayDadosTecnico = $this->tecnicoDAO->listarTodos();
-        for ($i = 0; $i < count($arrayDadosTecnico); $i++) {
-            $dadosTecnico = $arrayDadosTecnico[$i];
-            $arraySelect[] = "<option value=\"" . $dadosTecnico->__getIdTecnico() . "\">"
-                    . $dadosTecnico->__getNome() . "</option>";
+    public function _listCoachesForSelect(){
+        $coachData = new Coach();
+        $arrayCoachData = $this->coachDAO->listAllCoaches();
+        for ($i = 0; $i < count($arrayCoachData); $i++) {
+            $coachData = $arrayCoachData[$i];
+            $arraySelect[] = "<option value=\"" . $coachData->__getIdCoach() . "\">"
+                    . $coachData->__getCoachName() . "</option>";
         }
         return $arraySelect;
     }
@@ -33,80 +33,81 @@ class TecnicoController {
     /*
         List the function responsible for the technical data in the table.
      */
-    public function _listarTecnicosParaTabela(){
-        $dadosTecnico = new Tecnico();
-        $arrayDadosTecnico = $this->tecnicoDAO->listarTodos();
-        for ($i = 0; $i < count($arrayDadosTecnico); $i++){
-            $dadosTecnico = $arrayDadosTecnico[$i];
-            $arrayTr[] = " 
+    public function _listAllCoachesForTable(){
+        $coachData = new Coach();
+        $arrayCoachData = $this->coachDAO->listAllCoaches();
+        for ($i = 0; $i < count($arrayCoachData); $i++){
+            $coachData = $arrayCoachData[$i];
+            $htmlDataFromCoach[] = " 
 			<tr>
-          			<td><input type=\"checkbox\" value=\"1\" name=\"marcar[]\" /></td>
-					<td>" . $dadosTecnico->__getIdTecnico() . "</td>
-          			<td>" . $dadosTecnico->__getNome() . "</td>
-          			<td>" . $dadosTecnico->__getTelefone() . "</td>
-          			<td>" . $dadosTecnico->__getCpf() . "</td>
+                            <td><input type=\"checkbox\" value=\"1\" name=\"marcar[]\" /></td>
+				<td>" . $coachData->__getIdCoach() . "</td>
+          			<td>" . $coachData->__getCoachName() . "</td>
+          			<td>" . $coachData->__getCoachPhone() . "</td>
+          			<td>" . $coachData->__getCoachCpf() . "</td>
           			<td>
-            			<a href=\"?pag=tecnico&action=edit&id=" . $dadosTecnico->__getIdTecnico() .
-                    "\"><img src=\"./views/images/edit.png\" width=\"16\" height=\"16\" /></a>
-            			<a href=\"?pag=tecnico&action=exclude&id=" . $dadosTecnico->__getIdTecnico() . 
-                    "\"><img src=\"./views/images/delete.png\" width=\"16\" height=\"16\" /></a>
+            			<a href=\"?pag=tecnico&action=edit&id=" . 
+                                           $coachData->__getIdCoach() .
+                        "\"><img src=\"./views/images/edit.png\" width=\"16\" height=\"16\" /></a>
+            			<a href=\"?pag=tecnico&action=exclude&id=" . 
+                                           $coachData->__getIdCoach() . 
+                        "\"><img src=\"./views/images/delete.png\" width=\"16\" height=\"16\" /></a>
           			</td>
-			</tr>";
+                    </tr>";
         }
-        return $arrayTr;
+        return $htmlDataFromCoach;
     }
 
     /*
         Function responsible for performing a query by ID.
     */
-    public function _consultarPorId($id){
-        $dadosTecnico = new Tecnico();
-        $dadosTecnico = $this->tecnicoDAO->consultarPorId($id);
-        $arrayDados['nome'] = $dadosTecnico->__getNome();
-        $arrayDados['telefone'] = $dadosTecnico->__getTelefone();
-        $arrayDados['cpf'] = $dadosTecnico->__getCpf();
+    public function _consultByIdCoach($id){
+        $coachData = new Coach();
+        $coachData = $this->coachDAO->consultByIdCoach($id);
+        $arrayData['coachName'] = $coachData->__getCoachName();
+        $arrayData['coachPhone'] = $coachData->__getCoachPhone();
+        $arrayData['coachCpf'] = $coachData->__getCoachCpf();
 
-        return $arrayDados;
+        return $arrayData;
     }
 
     /*
         Function responsible for performing a query by name.
      */
-    public function _consultarPorNome($nome){
-        return $this->tecnicoDAO->consultarPorNome($nome);
+    public function _consultByCoachName($coachName){
+        return $this->coachDAO->consultByCoachName($coachName);
     }
 
     /*
         Function responsible for saving the data of a new techical already registered.
      */
-    public function _salvar($nome, $telefone, $cpf){
-        $dadosTecnico = new Tecnico();
-        $dadosTecnico->__constructOverload(0, $nome, $telefone, $cpf);
-        $this->tecnicoDAO->inserir($dadosTecnico);
+    public function _saveCoach($coachName, $coachPhone, $coachCpf){
+        $coachData = new Coach();
+        $coachData->__constructOverload(0, $coachName, $coachPhone, $coachCpf);
+        $this->coachDAO->insertCoach($coachData);
     }
 
     /*
         Function responsible for updating the data of technical.
     */
-    public function _atualizar($idTecnico, $nome, $telefone, $cpf){
-        $dadosTecnico = new Tecnico();
-        $dadosTecnico->__constructOverload($idTecnico, $nome, $telefone, $cpf);
-        $this->tecnicoDAO->atualizar($dadosTecnico);
+    public function _updateCoach($idCoach, $coachName, $coachPhone, $coachCpf){
+        $coachData = new Coach();
+        $coachData->__constructOverload($idCoach, $coachName, $coachPhone, $coachCpf);
+        $this->coachDAO->updateCoach($coachData);
     }
 
     /*
         Function responsible for deleting the data on technical.
      */
 
-    public function _excluir($id){
-        return $this->tecnicoDAO->excluir($id);
+    public function _deleteCoach($id){
+        return $this->coachDAO->deleteCoach($id);
     }
 
     /*
         Function responsible for counting how many records technicians.
     */
-    public function _contarRegistrosTecnico(){
-        return $this->tecnicoDAO->contarRegistrosTecnico();
+    public function _amountCoachesRecords(){
+        return $this->coachDAO->amountCoachesRecords();
     }
-
 }
