89965e8a4cb42adf815d91020bdbe0cc9fb527b3
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-05-23 10:47:09 -0300

    Refactoring the code in Team.php

--- model/Team.php
+++ model/Team.php
@@ -31,6 +31,11 @@ class Team{
     */
     public function __constructOverload($idTeam, $idCoach, $teamName, $teamCategory, $teamAddress, 
                                         $teamFoundationDate, $teamPresident, $teamPhone){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute.
+        */
 	$this->idTeam = $idTeam;
 	$this->idCoach = $idCoach;
 	$this->teamName = $teamName;
@@ -45,6 +50,10 @@ class Team{
         Method to verify the attribute 'idTeam' of the class.
     */
     public function __getIdTeam(){
+        
+        /* 
+            Return the value in the attribute 'idTeam'.
+        */
 	return $this->idTeam;
     }
         
@@ -52,6 +61,11 @@ class Team{
         Method to modify the attribute 'idTeam' of the class.
     */
     public function __setIdTeam($idTeam){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'idTeam'.
+        */
 	$this->idTeam = $idTeam;
     }
         
@@ -59,6 +73,10 @@ class Team{
         Method to verify the attribute 'idCoach' of the class.
     */
     public function __getIdCoach(){
+        
+        /* 
+            Return the value in the attribute 'idCoach'.
+        */
 	return $this->idCoach;
     }
         
@@ -66,6 +84,11 @@ class Team{
         Method to modify the attribute 'idCoach' of the class.
     */
     public function __setIdCoach($idCoach){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'idCoach'.
+        */
 	$this->idCoach = $idCoach;
     }
         
@@ -73,6 +96,10 @@ class Team{
         Method to verify the attribute 'teamName' of the class.
     */
     public function __getTeamName(){
+        
+        /* 
+            Return the value in the attribute 'teamName'.
+        */
 	return $this->teamName;
     }
         
@@ -80,6 +107,11 @@ class Team{
         Method to modify the attribute 'teamName' of the class.
     */
     public function __setTeamName($teamName){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamName'.
+        */
 	$this->teamName = $teamName;
     }
         
@@ -87,6 +119,10 @@ class Team{
         Method to verify the attribute 'teamCategory' of the class.
     */
     public function __getTeamCategory(){
+        
+        /* 
+            Return the value in the attribute 'teamCategory'.
+        */
 	return $this->teamCategory;
     }
         
@@ -94,6 +130,11 @@ class Team{
         Method to modify the attribute 'teamCategory' of the class.
     */
     public function __setTeamCategory($teamCategory){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamCategory'.
+        */
 	$this->teamCategory = $teamCategory;
     }	
 	
@@ -101,6 +142,10 @@ class Team{
         Method to verify the attribute 'teamAddress' of the class.
     */
     public function __getTeamAddress(){
+        
+        /* 
+            Return the value in the attribute 'teamAddress'.
+        */
     	return $this->teamAddress;
     }
         
@@ -108,6 +153,11 @@ class Team{
         Method to modify the attribute 'teamAddress' of the class.
     */
     public function __setTeamAddress($teamAddress){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamAddress'.
+        */
 	$this->teamAddress = $teamAddress;
     }
         
@@ -115,6 +165,10 @@ class Team{
         Method to verify the attribute 'teamFoundationDate' of the class.
     */
     public function __getTeamFoundationDate(){
+        
+        /* 
+            Return the value in the attribute 'teamFoundationDate'.
+        */
 	return $this->teamFoundationDate;
     }
         
@@ -122,6 +176,11 @@ class Team{
         Method to modify the attribute 'teamFoundationDate' of the class.
     */
     public function __setTeamFoundationDate($teamFoundationDate){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamFoundationDate'.
+        */
 	$this->teamFoundationDate = $teamFoundationDate;
     }
         
@@ -129,6 +188,10 @@ class Team{
         Method to verify the attribute 'teamPresident' of the class.
     */
     public function __getTeamPresident(){
+        
+        /* 
+            Return the value in the attribute 'teamPresident'.
+        */
 	return $this->teamPresident;
     }
         
@@ -136,6 +199,11 @@ class Team{
         Method to modify the attribute 'teamPresident' of the class.
     */
     public function __setTeamPresident($teamPresident){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamPresident'.
+        */
 	$this->teamPresident = $teamPresident;
     }
         
@@ -143,6 +211,10 @@ class Team{
         Method to verify the attribute 'teamPhone' of the class.
     */
     public function __getTeamPhone(){
+        
+        /* 
+            Return the value in the attribute 'teamPhone'.
+        */
 	return $this->teamPhone;
     }
         
@@ -150,6 +222,11 @@ class Team{
         Method to modify the attribute 'teamPhone' of the class.
     */
     public function __setTeamPhone($teamPhone){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamPhone'.
+        */
 	$this->teamPhone = $teamPhone;
     }
         
@@ -157,6 +234,10 @@ class Team{
         Method to verify the attribute 'teamPoints' of the class.
     */
     public function __getTeamPoints(){
+        
+        /* 
+            Return the value in the attribute 'teamPoints'.
+        */
 	return $this->teamPoints;
     }
         
@@ -164,6 +245,11 @@ class Team{
         Method to modify the attribute 'teamPoints' of the class.
     */
     public function __setTeamPoints($teamPoints){
+        
+        /*
+            This defines that the new value that the program or the user put on the attribute will 
+            be now the value of the attribute, in this case 'teamPoints'.
+        */
 	$this->teamPoints = $teamPoints;
     }
 }
