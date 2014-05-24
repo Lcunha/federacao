bbc0ba7b504d8e533330bee84de6839aa70e683a
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2014-05-23 21:10:08 -0300

    Refactoring the code in PlayerDAO.php

--- persistence/PlayerDAO.php
+++ persistence/PlayerDAO.php
@@ -21,8 +21,10 @@ class PlayerDAO{
 	}
 	
 	/*
-            The method is responsibility by list all registers in database table.
-	*/
+            The method is responsible to list all records about the players in database table.
+            This function doesn't have input parameters and returns a list with all the players 
+            data that already are in the database table.
+        */
 	public function listAllPlayers(){
             $sql = "SELECT * FROM jogador";
             $result = $this->connection->dataBase->Execute($sql);
@@ -38,8 +40,10 @@ class PlayerDAO{
 	}
 	
 	/*
-            The method is responsibility by to insert data in database table.
-	*/
+            The method is responsible to insert data in database table. This function have one
+            variable of the class Player (that capture the information with the getters method of 
+            the class to insert the information) as input parameter. It doesn't have any return.
+        */
 	public function insertPlayer(Player $playerData){
             $sql = "INSERT INTO jogador (time_id_time, nome, data_nascimento, cpf, numero) 
                     VALUES ('{$playerData->__getPlayerName()}','{$playerData->__getIdTeam()}',
@@ -48,9 +52,12 @@ class PlayerDAO{
             $this->connection->dataBase->Execute($sql);
 	}
 	
-	/*
-            The method is responsibility by to update data in database table.
-	*/
+	/* 
+            The method is responsible to update data in database table. This function have one
+            variable of the class Player (that capture the information with the getters method of 
+            the class to update the information) as input parameter. It returns all the player data
+            that are in the database table updated.
+        */
 	public function updatePlayer(Player $playerData){
             $sql = "UPDATE jogador 
                     SET time_id_time = '{$playerData->__getIdTeam()}',
@@ -64,8 +71,10 @@ class PlayerDAO{
 	}
 	
 	/*
-            The method is responsibility by to consult in database table through of ID.
-	*/
+            The method is responsible to consult by id the database table. This function have 
+            the id of the class Player (to search the database table) as input parameter. It 
+            returns all the player data that are in the database table searched.
+        */
 	public function consultByIdPlayer(Player $idPlayer){
             $sql = "SELECT * FROM jogador WHERE id_jogador = '{$idPlayer}'";
             $result = $this->connection->dataBase->Execute($sql);
@@ -78,8 +87,10 @@ class PlayerDAO{
 	}
 	
 	/*
-            The method is responsibility by to consult in database table through of Name.
-	*/
+            The method is responsible to consult by id the database table. This function have 
+            the idPlayer of the class Player (to search the database table) as input parameter. It 
+            returns all the player data that are in the database table searched.
+        */
 	public function consultByPlayerName(Player $playerName){
             $sql = "SELECT * FROM jogador WHERE nome = '{$playerName}'";
             $result = $this->connection->dataBase->Execute($sql);
@@ -92,8 +103,10 @@ class PlayerDAO{
 	}
 
 	/*
-            The method is responsibility by to consult in database table through of data Team.
-	*/
+            The method is responsible to consult by id the database table. This function have 
+            the idTeam of the class Player (to search the database table) as input parameter. It 
+            returns all the player data that are in the database table searched.
+        */
 	public function consultPlayerByIdTeam(Player $teamData){
             $sql = "SELECT * FROM jogador WHERE time_id_time = '{$teamData->__getIdTeam()}'";
             $result = $this->connection->dataBase->Execute($sql);
@@ -105,16 +118,18 @@ class PlayerDAO{
             return $playerData;
 	}
 
-	/*
-            The mthod is responsibility by to delete data in databse table.
-	*/
+	/* 
+            The method is responsible to delete in database table through of id. This function have 
+            the id of the class Player as input parameter. It doesn't have return.
+        */
 	public function deletePlayer(Player $idPlayer){
             $sql = "DELETE FROM jogador WHERE id_jogador = '{$idPlayer}' ";
             $result = $this->connection->dataBase->Execute($sql);
 	}
 
 	/*
-            The method is responsibility by count all registers of type Player.
+            The method is responsible to count all records of the players. This function doesn't
+            have input parameter and returns the number of records.
 	*/
 	public function amountPlayersRecords(){
             $sql = "SELECT COUNT(*) as contagem FROM jogador";
@@ -124,8 +139,10 @@ class PlayerDAO{
 	}
 
 	/*
-            The method is responsibility by to list gunners through of information data of type Player and Team.
-	*/
+            The method is responsible to list all records about the gunners in database table.
+            This function doesn't have input parameters and returns a list with all the gunners 
+            data that already are in the database table.
+        */
 	public function listGunners(){
             $sql = "SELECT j.nome AS nome,      
                            t.nome AS nome_time, 
@@ -148,8 +165,10 @@ class PlayerDAO{
 	}
 
 	/*
-            The method is responsibility by to list fear players through of information data of type Player and Team.
-	*/
+            The method is responsible to list all records about the fear players in database table.
+            This function doesn't have input parameters and returns a list with all the fear 
+            players data that already are in the database table.
+        */
 	public function listFearPlayers(){
                     $sql = "SELECT j.nome AS nome,
                            t.nome AS nome_time,
