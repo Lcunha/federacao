1a485da67b3f67e059871b929286f6e2aaabb946
caiquepereira <caiquepereira@gmail.com>
2014-05-23 21:18:06 -0300

    Improved functions comments on view/MainView.php

--- view/MainView.php
+++ view/MainView.php
@@ -28,42 +28,49 @@ class MainView{
 		$this->refereeCO = new RefereeController();
 	}
         /*
+            The function returns to the team controller the function.
             This function is responsible to count the team records.
         */
 	public function amountTeamsRecords(){
 		return $this->teamCO->_amountTeamsRecords();
 	}
         /*
+            The function returns to the player controller the function.
             This function is responsible to count the player records.
         */
 	public function amountPlayersRecords(){
 		return $this->playerCO->_amountPlayersRecords();
 	}
         /*
+            The function returns to the coach controller the function.
             This function is responsible to count the coach records.
         */
 	public function amountCoachesRecords(){
 		return $this->coachCO->_amountCoachesRecords();
 	}
         /*
+            The function returns to the referee controller the function.
             This function is responsible to count the Referee records.
         */
 	public function amountRefereesRecords(){
 		return $this->refereeCO->_amountRefereesRecords();
 	}
         /*
+            The function returns to the player controller the function.
             This function is responsible to list the Artilheiros.
         */
 	public function listGunners(){
 		return $this->playerCO->_listGunners();
 	}
         /*
+            The function returns to the player controller the function.
             This function is responsible to list the fear players.
         */
 	public function listFearPlayers(){
 		return $this->playerCO->_listFearPlayers();
 	}
         /*
+            The function returns to the team controller the function.
             This function is responsible to list teams to the championship table.
         */
 	public function listAllTeamsForChampionshipTable(){
