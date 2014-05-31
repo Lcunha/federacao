a390ea2913fe35ba54c3fd8d6ef2f70b77495627
Larissa <larissax55@gmail.com>
2014-05-31 16:17:13 -0300

    Finishing the tests in classe DadosTeamTest.php

--- test/model/DataTeamTest.php
+++ test/model/DataTeamTest.php
@@ -39,6 +39,36 @@ class DataTeamTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals('felipe',$result);
     }
     
+     public function testSetVictoryTeam() {
+        $this->dataTeamTest->__setVictoryTeam(5);
+        $result = $this->dataTeamTest->__getVictoryTeam();
+        $this->assertEquals(5 ,$result);
+    }
+    
+     public function testSetDrawTeam() {
+        $this->dataTeamTest->__setDrawTeam(3);
+        $result = $this->dataTeamTest->__getDrawTeam();
+        $this->assertEquals(3 ,$result);
+    }
+    
+     public function testSetLossTeam() {
+        $this->dataTeamTest->__setLossTeam(1);
+        $result = $this->dataTeamTest->__getLossTeam();
+        $this->assertEquals(1 ,$result);
+    }
+    
+     public function testSetAmountGoals() {
+        $this->dataTeamTest->__setAmountGoals(7);
+        $result = $this->dataTeamTest->__getAmountGoals();
+        $this->assertEquals(7 ,$result);
+    }
+    
+     public function testSetConcededGoals() {
+        $this->dataTeamTest->__setConcededGoals(9);
+        $result = $this->dataTeamTest->__getConcededGoals();
+        $this->assertEquals(9 ,$result);
+    }
+    
 }
 
 
