3b439510e1cd43eddcbb59206a679b5ab4a34276
caiquepereira <caiquepereira@gmail.com>
2014-06-03 18:07:33 -0300

    Finished creating tests to model/Game.php

--- test/model/GameTest.php
+++ test/model/GameTest.php
@@ -37,20 +37,32 @@ class GameTest extends PHPUnit_Framework_TestCase {
     public function testSetGameLocation(){
         $this->gameTest->__setGameLocation('Gama');
         $result = $this->gameTest->__getGameLocation();
-        $this->assertEquals('Gama',$result);    
+        $this->assertEquals('Gama',$result);           
+    }
+    
+    public function testSetGameDate(){
+        $this->gameTest->__setGameDate('2013-12-30');
+        $result = $this->gameTest->__getGameDate();
+        $this->assertEquals('2013-12-30',$result);    
+    }
+    
+    public function testSetGameTime(){
+        $this->gameTest->__setGameTime('23:59:00');
+        $result = $this->gameTest->__getGameTime();
+        $this->assertEquals('23:59:00',$result);
         
     }
-    /*
-    public function __setGameDate($gameDate){
+    
+    public function testSetGameDuration(){
+        $this->gameTest->__setGameDuration(1);
+        $result = $this->gameTest->__getGameDuration();
+        $this->assertEquals(1,$result);
     }
     
-    public function __setGameTime($gameTime){
+    public function testSetTotal7Meters(){
+        $this->gameTest->__setTotal7Meters(7);
+        $result = $this->gameTest->__getTotal7Meters();
+        $this->assertEquals(7,$result);       
     }
     
-    public function __setGameDuration($gameDuration){
-    }
-    
-    public function __setTotal7Meters($total7Meters){
-    }
-    */
 }
