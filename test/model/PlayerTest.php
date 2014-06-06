b9c6ea36e6b4687a5a31aebdc429829174b0dbd3
Lcunha <lucasfilipe2007@hotmail.com>
2014-06-06 10:44:15 -0300

    [Test] Add initial functions of test in test/model/PlayerTest.php

--- test/model/PlayerTest.php
+++ test/model/PlayerTest.php
@@ -5,6 +5,14 @@ include_once("/../../model/Player.php");
 class PlayerTest extends PHPUnit_Framework_TestCase{
 	
 	protected $playerTest;
+
+	protected function setUp() {
+		$this->playerTest = new Player();
+	}
+
+	protected function tearDown(){
+		unset($this->playerTest);
+	}
 }
 
 ?>
