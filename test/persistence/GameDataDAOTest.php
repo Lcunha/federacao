720ddf803609858769314df026be1deb72423860
larissa <larissax55@gmail.com>
2014-06-06 22:03:07 -0300

    Created test in class GameDataDAOTest.php

--- test/persistence/GameDataDAOTest.php
+++ test/persistence/GameDataDAOTest.php
@@ -17,6 +17,35 @@ class GameDataDAOTest extends PHPUnit_Framework_TestCase{
         unset($this->gameDataDAOTest);
         unset($this->gameDataTest);
     }
+    
+     public function testGetInstanceGameDAtaDaoTestTestNotNull() {
+        $this->assertNotNull($this->gameDataDAOTest);
+    }
+    
+      public function testGetInstanceGameDataTestNotNull() {
+        $this->assertNotNull($this->gameDataTest);
+    }
+    
+    public function testInsertGameData() {
+        $retorno = $this->gameDataDAOTest->insertGameData($this->gameDataTest);
+        $this->assertTrue($retorno);
+    }    
+
+    public function testUpdateData() {
+        $retorno = $this->gameDataDAOTest->insertGameData($this->gameDataTest);
+        $this->assertTrue($retorno);
+    } 
+    
+    public function testUpdateGameData() {
+        $retorno = $this->gameDataDAOTest->insertGameData($this->gameDataTest);
+        $this->assertTrue($retorno);
+    } 
+   
+    public function testDeleteGameData() {
+        $retorno = $this->gameDataDAOTest->deleteGameData(1);
+        $this->assertTrue($retorno);
+    } 
+    
 
 }
 ?>
