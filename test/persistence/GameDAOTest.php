dedce73107879aa344f8b3c6fd1afb0b26344989
caiquepereira <caiquepereira@gmail.com>
2014-06-09 23:32:37 -0300

    Tested two methods of persistence/TeamGameDAO.php and made other changes

--- test/persistence/GameDAOTest.php
+++ test/persistence/GameDAOTest.php
@@ -8,7 +8,7 @@ class GameDAOTest extends PHPUnit_Framework_TestCase {
 
    
     protected function setUp() {
-        $this->gameTest = new Game(/*parametros*/);
+        $this->gameTest = new Game(2, 0, '1', '1', '2013-12-31', '23:59:00', 23, 0);
         $this->gameDaoTest = new GameDAO();
     }
 
@@ -18,32 +18,38 @@ class GameDAOTest extends PHPUnit_Framework_TestCase {
         
     }
     
-    
+    /*
     public function listAllGames(){
-            
+            $return = $this->refereeDaoTest->listAllReferees();
+        $this->assertNotNull($return); 
     }
 	
     public function insertGame(Game $gameData){
-            
+            $return = $this->refereeDaoTest->insertReferee($this->refereeTest);
+        $this->assertTrue($return);
     }
 	
     public function updateGame(Game $gameData){
-            
+            $return = $this->refereeDaoTest->updateReferee($this->refereeTest);
+        $this->assertTrue($return);
     }
 	
 	
     public function consultByIdGame(Game $idGame){
-            
+            $return = $this->refereeDaoTest->consultByIdReferee(1);
+        $this->assertFalse($return);
     }
 	
 	
     public function consultByGameDate(Game $gameDate){
-            
+            $return = $this->refereeDaoTest->consultByRefereeName('JoÃ£o de Barro');
+        $this->assertFalse($return);     
     }
 
 	
     public function deleteGame(Game $idReferee){
-           
+           $retorno = $this->refereeDaoTest->deleteReferee(15);
+        $this->assertTrue($retorno);  
     }
-    
+    */
 }
--- test/persistence/TeamGameDAOTest.php
+++ test/persistence/TeamGameDAOTest.php
@@ -7,7 +7,7 @@ include_once("/../../persistence/Connection.php");
 class TeamGameDAOTest extends PHPUnit_Framework_TestCase {
     
     protected function setUp() {
-        $this->teamGameTest = new TeamGame(/*parametros*/);
+        $this->teamGameTest = new TeamGame(0, 1);
         $this->teamGameDaoTest = new TeamGameDAO();
     }
 
@@ -17,32 +17,38 @@ class TeamGameDAOTest extends PHPUnit_Framework_TestCase {
         
     }
     
-    public function listAllTeamGames(){
-            
+    public function testListAllTeamGames(){
+        $return = $this->teamGameDaoTest->listAllTeamGames();
+        $this->assertNotNull($return);    
     }
 
-    public function insertTeamGame(TeamGame $teamDataGame){
-          
+    public function testInsertTeamGame(){
+        $return = $this->teamGameDaoTest->insertTeamGame($this->teamGameTest);
+        $this->assertTrue($return);
     }
 
-	
+    /*
     public function updateTeamGame(TeamGame $teamDataGame){
-            
+            $return = $this->refereeDaoTest->updateReferee($this->refereeTest);
+        $this->assertTrue($return);
     }
 
 	
     public function consultByIdTeam(TeamGame $idTeam){
-           
+           $return = $this->refereeDaoTest->consultByIdReferee(1);
+        $this->assertFalse($return);
     }
 	
     public function consultByTime(TeamGame $idTimePlay){
-           
+           $return = $this->refereeDaoTest->consultByRefereeName('JoÃ£o de Barro');
+        $this->assertFalse($return);     
     }
 
 	
     public function deleteTeamGame(TeamGame $idGame){
-            
+            $retorno = $this->refereeDaoTest->deleteReferee(15);
+        $this->assertTrue($retorno);  
     }
     
-    
+    */
 }
